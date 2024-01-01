ce3a6d8b2296967dbe584ac7f0c73ca186629ad2
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-01-01 17:32:27 +0300

    COmpany model

--- django_pos/company/models.py
+++ django_pos/company/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    phone_number = models.CharField(max_length=20)  # Updated field type
+    email = models.EmailField(blank=True)
+    city = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    logo=models.ImageField(blank=False, null=False)
+    company_name = models.CharField(max_length=100, blank=False, null=False)
+
+    def __str__(self) -> str:
+        return self.company_name
