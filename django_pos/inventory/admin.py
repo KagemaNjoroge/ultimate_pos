1585befa66b7968d0d7b3749751205d976b3401d
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-01-06 16:57:43 +0300

    Register Inventory Model

--- django_pos/inventory/admin.py
+++ django_pos/inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Inventory
 # Register your models here.
+
+
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ['product', 'quantity', 'date_added', 'date_modified']
+
+admin.site.register(Inventory, InventoryAdmin)
