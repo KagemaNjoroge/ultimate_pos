15da290e84f1c7d94db328f94d56726316239308
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-01-06 16:54:49 +0300

    Stock model

--- django_pos/inventory/models.py
+++ django_pos/inventory/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from products.models import Product
 # Create your models here.
+
+
+class Inventory(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    date_added = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.product} - {self.quantity}"
+    
+    def update_stock(self, quantity):
+        self.quantity += quantity
+        self.save()
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "product": self.product.name,
+            "quantity": self.quantity,
+            "date_added": self.date_added,
+            "date_modified": self.date_modified
+        }
