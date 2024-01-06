c6ce53899ff6adc759eef12cd9ac8e51f2d91a99
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-01-06 20:03:14 +0300

    Inventory index page

--- django_pos/inventory/views.py
+++ django_pos/inventory/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from inventory.models import Inventory
+
 # Create your views here.
+def index(request):
+    inventory = Inventory.objects.all()
+    context = {
+        'inventory': inventory,
+        "active_icon": "inventory"
+    }
+    return render(request, 'inventory/inventory.html', context)
