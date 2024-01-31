d282cb17127e356caf8abc8b38f30e5f57f76b02
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-01-31 23:07:56 +0300

    Add endpoint to add new inventory

--- django_pos/inventory/urls.py
+++ django_pos/inventory/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, add_inventory
 
 app_name = 'inventory'
 urlpatterns = [
     path('', index, name="inventory_index"),
+    path('add/', add_inventory, name="inventory_add")
 ]
