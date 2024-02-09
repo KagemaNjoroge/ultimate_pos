7a1010699e296f8d66d821349d2e4cb4476ff4fc
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-02-09 19:00:57 +0300

    Reformat file

--- django_pos/company/models.py
+++ django_pos/company/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
 class Company(models.Model):
     phone_number = models.CharField(max_length=20)  # Updated field type
     email = models.EmailField(blank=True)
     city = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
-    logo=models.ImageField(blank=False, null=False)
+    logo = models.ImageField(blank=False, null=False)
     company_name = models.CharField(max_length=100, blank=False, null=False)
-    currency_symbol=models.CharField(max_length=5, default='Kshs')
+    currency_symbol = models.CharField(max_length=5, default='Kshs')
 
     def __str__(self) -> str:
         return self.company_name
-    
+
     def to_dict(self) -> dict:
         return {
             "phone": self.phone_number,
@@ -23,3 +24,7 @@ class Company(models.Model):
             "company_name": self.company_name,
             "currency": self.currency_symbol
         }
+
+    class Meta:
+        verbose_name_plural = 'Companies'
+        verbose_name = 'Company'
