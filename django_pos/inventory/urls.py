8477ca126e380f982d959d0b1eadb39d8fafa150
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-02-10 13:26:29 +0300

    Add update inventory endpoint

--- django_pos/inventory/urls.py
+++ django_pos/inventory/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, add_inventory
+from .views import index, add_inventory, update_inventory
 
 app_name = 'inventory'
 urlpatterns = [
     path('', index, name="inventory_index"),
-    path('add/', add_inventory, name="inventory_add")
+    path('add/', add_inventory, name="inventory_add"),
+    path('update/<str:inventory_id>', update_inventory, name="update_inventory")
 ]
