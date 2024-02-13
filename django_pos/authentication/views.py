7bd8cc0fc158617caffbbac8b25fed31f12ecfc9
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-02-13 15:05:22 +0300

    Fix typo and format code

--- django_pos/authentication/views.py
+++ django_pos/authentication/views.py
@@ -8,26 +8,24 @@ from django.views.decorators.http import require_http_methods
 from django.contrib import messages
 
 
-
-
-def logout_view(request:HttpRequest)->HttpResponse:
-
+def logout_view(request: HttpRequest) -> HttpResponse:
     logout(request)
     return redirect('/')
 
+
 @login_required(login_url="/accounts/login/")
 @require_http_methods(['GET', 'POST'])
-def profile(request:HttpRequest)->HttpResponse:
+def profile(request: HttpRequest) -> HttpResponse:
     if request.method == 'GET':
         return render(request, "accounts/profile.html")
     else:
-        
-        email=request.POST.get('email')
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        user = request.user   
+
+        email = request.POST.get(key='email')
+        first_name = request.POST.get(key='first_name')
+        last_name = request.POST.get(key='last_name')
+        user = request.user
         # if email != '' and first_name != '' and last_name != '':
-            
+
         #     user.email = email
         #     user.first_name = first_name
         #     user.last_name = last_name
@@ -37,11 +35,11 @@ def profile(request:HttpRequest)->HttpResponse:
         #     return redirect('/accounts/profile')
         if email == '':
             email = None
-        if first_name== '':
+        if first_name == '':
             first_name = None
-        if last_name=='':
-            last_name=None
-        
+        if last_name == '':
+            last_name = None
+
         user.email = email or user.email
         user.first_name = first_name or user.first_name
         user.last_name = last_name or user.last_name
@@ -49,12 +47,8 @@ def profile(request:HttpRequest)->HttpResponse:
         user.save()
         messages.add_message(request=request, message='Profile updated successfully', level=messages.SUCCESS)
         return redirect('/accounts/profile')
-        
-
-        
 
 
-        
 def login_view(request):
     form = LoginForm(request.POST or None)
 
@@ -72,7 +66,7 @@ def login_view(request):
             else:
                 msg = 'Invalid username or password!'
         else:
-            msg = 'An error ocurred!.'
+            msg = 'An error occurred!.'
 
     return render(request, "accounts/login.html", {"form": form, "msg": msg})
 
@@ -87,7 +81,7 @@ def register_user(request):
             form.save()
             username = form.cleaned_data.get("username")
             raw_password = form.cleaned_data.get("password")
-            user = authenticate(username=username, password=raw_password)
+            authenticate(username=username, password=raw_password)
 
             msg = 'User created - please <a href="/login">login</a>.'
             success = True
