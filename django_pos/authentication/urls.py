f5b9b4896f2034988fb1e485256054b718663114
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-02-19 14:51:09 +0300

    Format code

--- django_pos/authentication/urls.py
+++ django_pos/authentication/urls.py
@@ -5,6 +5,6 @@ app_name = "authentication"
 urlpatterns = [
     path('accounts/login/', login_view, name="login"),
     path('accounts/register/', register_user, name="register"),
-    path('accounts/logout/',logout_view , name="logout"),
+    path('accounts/logout/', logout_view, name="logout"),
     path('accounts/profile/', profile, name='profile')
 ]
