8b0a71ac33c1170ff6db51f525e3655dbb16eb05
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-03-02 16:25:11 +0300

    Refactor Customer model in django_pos/customers/models.py

--- django_pos/customers/models.py
+++ django_pos/customers/models.py
@@ -2,27 +2,42 @@ from django.db import models
 
 
 class Customer(models.Model):
+    """
+    Represents a customer in the system.
+    """
+
     first_name = models.CharField(max_length=256)
     last_name = models.CharField(max_length=256, blank=True, null=True)
     address = models.TextField(max_length=256, blank=True, null=True)
     email = models.EmailField(max_length=256, blank=True, null=True)
     phone = models.CharField(max_length=15, blank=True, null=True)
-    kra_pin = models.CharField(max_length=12, default="A000000000Z", blank=True, null=True)
+    kra_pin = models.CharField(
+        max_length=12, default="A000000000Z", blank=True, null=True
+    )
 
     class Meta:
-        db_table = 'Customers'
-        verbose_name_plural = 'Customers'
-        verbose_name = 'Customer'
+        db_table = "Customers"
+        verbose_name_plural = "Customers"
+        verbose_name = "Customer"
 
     def __str__(self) -> str:
-        return f'{self.first_name} {self.last_name}'
+        return f"{self.first_name} {self.last_name}"
 
-    def get_full_name(self):
-        return f'{self.first_name} {self.last_name}'
+    def get_full_name(self) -> str:
+        """
+        Returns the full name of the customer.
 
-    def to_select2(self):
-        item = {
-            "label": self.get_full_name(),
-            "value": self.id
-        }
+        Returns:
+            str: The full name of the customer.
+        """
+        return f"{self.first_name} {self.last_name}"
+
+    def to_select2(self) -> dict:
+        """
+        Converts the customer object to a dictionary format compatible with Select2 library.
+
+        Returns:
+            dict: A dictionary containing the label and value of the customer object.
+        """
+        item = {"label": self.get_full_name(), "value": self.id}
         return item
