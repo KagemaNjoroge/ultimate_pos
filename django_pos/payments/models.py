a59310d5d1263ecd07fffaba1f20fc82268d04d5
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-03-02 16:41:33 +0300

    Refactor payment models and fix formatting

--- django_pos/payments/models.py
+++ django_pos/payments/models.py
@@ -5,21 +5,21 @@ from sales.models import Sale
 
 class PaymentMethod(models.Model):
     payment_choices = [
-        ('cash', 'Cash'),
-        ('credit', 'Credit Card'),
-        ('debit', 'Debit Card'),
-        ('mpesa', 'M-Pesa'),
-        ('cheque', 'Cheque'),
-        ('other', 'Other')
+        ("cash", "Cash"),
+        ("credit", "Credit Card"),
+        ("debit", "Debit Card"),
+        ("mpesa", "M-Pesa"),
+        ("cheque", "Cheque"),
+        ("other", "Other"),
     ]
     name = models.CharField(max_length=255, choices=payment_choices)
     description = models.TextField(blank=True, null=True)
-    logo = models.ImageField(upload_to='payment_methods', blank=True, null=True)
+    logo = models.ImageField(upload_to="payment_methods", blank=True, null=True)
 
     class Meta:
-        db_table = 'PaymentMethods'
-        verbose_name_plural = 'Payment Methods'
-        verbose_name = 'Payment Method'
+        db_table = "PaymentMethods"
+        verbose_name_plural = "Payment Methods"
+        verbose_name = "Payment Method"
 
     def __str__(self) -> str:
         return self.name
@@ -28,26 +28,26 @@ class PaymentMethod(models.Model):
         return {
             "name": self.name,
             "description": self.description,
-            "logo": self.logo.url if self.logo else None
+            "logo": self.logo.url if self.logo else None,
         }
 
 
 class Payments(models.Model):
-    sale = models.ForeignKey(
-        Sale, models.PROTECT, db_column='sale')
+    sale = models.ForeignKey(Sale, models.PROTECT, db_column="sale")
     payment_method = models.ForeignKey(
-        PaymentMethod, models.PROTECT, db_column='payment_method')
+        PaymentMethod, models.PROTECT, db_column="payment_method"
+    )
     amount = models.FloatField()
     date = models.DateTimeField(auto_now_add=True)
     reference = models.CharField(max_length=255, blank=True, null=True)
 
     class Meta:
-        db_table = 'Payments'
-        verbose_name_plural = 'Payments'
-        verbose_name = 'Payment'
+        db_table = "Payments"
+        verbose_name_plural = "Payments"
+        verbose_name = "Payment"
 
     def __str__(self) -> str:
-        return "Payment ID: " + str(self.id) + " Sale ID: " + str(self.sale.id) + " Amount: " + str(self.amount)
+        return f"Payment ID:  {self.id}  Sale ID: {self.sale.id} Amount: {self.amount}"
 
     def to_json(self) -> dict:
         return {
@@ -55,5 +55,5 @@ class Payments(models.Model):
             "payment_method": self.payment_method.name,
             "amount": self.amount,
             "date": self.date,
-            "reference": self.reference
+            "reference": self.reference,
         }
