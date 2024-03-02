21fcf0e19f68342432e3b7a1b9d5aeec3bc8365f
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-03-02 16:41:41 +0300

    Refactor code for calculating earnings and top-selling products

--- django_pos/pos/views.py
+++ django_pos/pos/views.py
@@ -22,21 +22,36 @@ def index(request: HttpRequest) -> HttpResponse:
 
     # Calculate earnings per month
     for month in range(1, 13):
-        earning = Sale.objects.filter(date_added__year=year, date_added__month=month).aggregate(
-            total_variable=Coalesce(Sum(F('grand_total')), 0.0, output_field=FloatField())).get('total_variable')
+        earning = (
+            Sale.objects.filter(date_added__year=year, date_added__month=month)
+            .aggregate(
+                total_variable=Coalesce(
+                    Sum(F("grand_total")), 0.0, output_field=FloatField()
+                )
+            )
+            .get("total_variable")
+        )
         monthly_earnings.append(earning)
 
     # Calculate annual earnings
-    annual_earnings = Sale.objects.filter(date_added__year=year).aggregate(total_variable=Coalesce(
-        Sum(F('grand_total')), 0.0, output_field=FloatField())).get('total_variable')
-    annual_earnings = format(annual_earnings, '.2f')
+    annual_earnings = (
+        Sale.objects.filter(date_added__year=year)
+        .aggregate(
+            total_variable=Coalesce(
+                Sum(F("grand_total")), 0.0, output_field=FloatField()
+            )
+        )
+        .get("total_variable")
+    )
+    annual_earnings = format(annual_earnings, ".2f")
 
     # AVG per month
-    avg_month = format(sum(monthly_earnings) / 12, '.2f')
+    avg_month = format(sum(monthly_earnings) / 12, ".2f")
 
     # Top-selling products
-    top_products = Product.objects.annotate(quantity_sum=Sum(
-        'saledetail__quantity')).order_by('-quantity_sum')[:3]
+    top_products = Product.objects.annotate(
+        quantity_sum=Sum("saledetail__quantity")
+    ).order_by("-quantity_sum")[:3]
 
     top_products_names = []
     top_products_quantity = []
@@ -59,56 +74,61 @@ def index(request: HttpRequest) -> HttpResponse:
 
     company = Company.objects.first()
     if company:
-        context['currency_symbol'] = company.currency_symbol
+        context["currency_symbol"] = company.currency_symbol
     else:
-        context['currency_symbol'] = "$"
+        context["currency_symbol"] = "$"
 
     return render(request, "pos/index.html", context)
 
 
 @login_required(login_url="/accounts/login/")
 def pos(request: HttpRequest) -> HttpResponse:
-    if request.method == 'GET':
+    if request.method == "GET":
         products = Product.objects.all()
         categories = Category.objects.all()
         customers = Customer.objects.all()
-        return render(request, "pos/pos.html", {"products": products, "categories": categories, "customers": customers})
-    elif request.method == 'POST':
+        return render(
+            request,
+            "pos/pos.html",
+            {"products": products, "categories": categories, "customers": customers},
+        )
+    elif request.method == "POST":
         try:
             sale_data = json.loads(request.body)
-            # {'customer': '1', 'items': [{'name': 'Porsche Cayenne', 'price': 4000000, 'count': 1, 'id': 9},
-            # {'name': 'Lenovo Flex 5', 'price': 60000, 'count': 1, 'id': 10}, {'name': 'Air Max 9', 'price': 10.99,
-            # 'count': 1, 'id': 6}, {'name': 'Odoo ERP & CMS', 'price': 90000, 'count': 1, 'id': 5}], 'grand_total':
-            # 4150010.99}
-            items = sale_data['items']
-            customer_id = sale_data['customer']
-            grand_total = float(sale_data['grand_total'])
-            amount_paid = float(sale_data['amount_paid'])
+            items = sale_data["items"]
+            customer_id = sale_data["customer"]
+            grand_total = float(sale_data["grand_total"])
+            amount_paid = float(sale_data["amount_paid"])
 
             change = amount_paid - grand_total
             customer = get_object_or_404(Customer, id=customer_id)
             # hard coded tax percentage TODO: make it dynamic as per a particular product
             tax_percentage = 16
             tax_amount = grand_total * tax_percentage / 100
-            sale = Sale.objects.create(customer=customer, grand_total=grand_total, amount_payed=amount_paid,
-                                       amount_change=change, tax_percentage=tax_percentage, tax_amount=tax_amount,
-                                       sub_total=grand_total - tax_amount)
+            sale = Sale.objects.create(
+                customer=customer,
+                grand_total=grand_total,
+                amount_payed=amount_paid,
+                amount_change=change,
+                tax_percentage=tax_percentage,
+                tax_amount=tax_amount,
+                sub_total=grand_total - tax_amount,
+            )
             sale.save()
 
             for item in items:
-                product = get_object_or_404(Product, id=item['id'])
+                product = get_object_or_404(Product, id=item["id"])
                 attr = {
                     "product": product,
                     "price": product.price,
-                    "quantity": item['count'],
-                    "total_detail": product.price * item['count'],
-                    "sale": sale
+                    "quantity": item["count"],
+                    "total_detail": product.price * item["count"],
+                    "sale": sale,
                 }
                 sale_detail = SaleDetail.objects.create(**attr)
                 sale_detail.save()
 
             return JsonResponse({"status": "success", "sale_id": sale.id})
         except Exception as e:
-            print(e)
             return JsonResponse({"status": "error", "error_message": str(e)})
     return HttpResponse("Invalid request", status=400)
