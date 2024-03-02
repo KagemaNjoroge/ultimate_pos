27b1f1986c36ceffe347ef2d139fb99002e5c92b
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-03-03 01:25:11 +0300

    Add Notifications model to pos app

--- django_pos/pos/models.py
+++ django_pos/pos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Notifications(models.Model):
+    title = models.CharField(max_length=100)
+    message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Notification"
+        verbose_name_plural = "Notifications"
+        ordering = ["-date"]
