dbf1f3b1663cbadbe01928a6514c5cb5625acc14
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-03-02 16:41:27 +0300

    Refactor inventory views and templates

--- django_pos/inventory/views.py
+++ django_pos/inventory/views.py
@@ -8,44 +8,37 @@ from products.models import Product
 # Create your views here.
 def index(request):
     inventory = Inventory.objects.all()
-    context = {
-        'inventory': inventory,
-        "active_icon": "inventory"
-    }
-    return render(request, 'inventory/inventory.html', context)
+    context = {"inventory": inventory, "active_icon": "inventory"}
+    return render(request, "inventory/inventory.html", context)
 
 
 def add_inventory(request: HttpRequest) -> HttpResponse:
-    if request.method == 'GET':
-        products = Product.objects.all()
-        inv = Inventory.objects.all()
-        for i in inv:
-            if i.product in products:
-                print(i.product.name)
-        return render(request, 'inventory/inventory_add.html', {"products": products})
-    elif request.method == 'POST':
+    if request.method == "GET":
+        # only products that are not in the inventory
+        products = Product.objects.exclude(inventory__isnull=False)
+        return render(request, "inventory/inventory_add.html", {"products": products})
+    elif request.method == "POST":
         data = json.loads(request.body)
-        product_id = int(data['product'])
-        quantity = int(data['quantity'])
-
+        product_id = int(data["product"])
+        quantity = int(data["quantity"])
         product = Product.objects.get(id=product_id)
         inventory = Inventory(product=product, quantity=quantity)
         try:
             inventory.save()
             return JsonResponse({"status": "success"}, safe=True)
         except Exception as ee:
-            print(str(ee))
             return JsonResponse({"status": "fail", "error": str(ee)}, safe=True)
 
 
 def update_inventory(request: HttpRequest, inventory_id: int) -> HttpResponse:
     inventory = get_object_or_404(Inventory, id=inventory_id)
-    if request.method == 'GET':
-
-        return render(request, 'inventory/inventory_update.html', {"inventory": inventory})
-    elif request.method == 'POST':
+    if request.method == "GET":
+        return render(
+            request, "inventory/inventory_update.html", {"inventory": inventory}
+        )
+    elif request.method == "POST":
         data = json.loads(request.body)
-        quantity = int(data['quantity'])
+        quantity = int(data["quantity"])
         inventory.quantity = quantity
         inventory.save()
         return JsonResponse({"status": "success"}, safe=True)
