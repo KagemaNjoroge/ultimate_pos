bf145d4a11bbd2b9dce9860d00d7f0d86acfbc23
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-03-04 16:41:10 +0300

    Add SaleItemAdmin to sales/admin.py

--- django_pos/sales/admin.py
+++ django_pos/sales/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Sale, SaleDetail
+from .models import Sale, SaleDetail, SaleItem
 
 
 class SaleAdmin(admin.ModelAdmin):
@@ -11,5 +11,10 @@ class SaleDetailAdmin(admin.ModelAdmin):
     list_display = ("id",)
 
 
+class SaleItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "product", "quantity")
+
+
+admin.site.register(SaleItem, SaleItemAdmin)
 admin.site.register(Sale, SaleAdmin)
 admin.site.register(SaleDetail, SaleDetailAdmin)
