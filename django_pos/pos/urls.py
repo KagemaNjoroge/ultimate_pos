79074a2af01a923ed1bcd8d9a799d47b1d6ac3ee
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-03-04 21:07:58 +0300

    Add new URLs for notifications

--- django_pos/pos/urls.py
+++ django_pos/pos/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 app_name = "pos"
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('pos/', views.pos, name='pos')
+    path("", views.index, name="index"),
+    path("pos/", views.pos, name="pos"),
+    path("notifications/", views.get_notifications, name="notifications"),
+    path("notifications/<int:id>/", views.get_notifications, name="notifications"),
 ]
