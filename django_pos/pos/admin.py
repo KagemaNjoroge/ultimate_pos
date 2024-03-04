2de43d330c45d25fef943c89996bdc637e5c383c
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-03-04 21:07:50 +0300

    Add NotificationsAdmin to admin.py

--- django_pos/pos/admin.py
+++ django_pos/pos/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
+from .models import Notifications
 
+
+class NotificationsAdmin(admin.ModelAdmin):
+    list_display = ("title", "date", "read", "user")
+    list_filter = ("date", "read")
+    search_fields = ("title", "message", "user__username")
+
+
+admin.site.register(Notifications, NotificationsAdmin)
 # Register your models here.
 admin.site.site_header = "Ultimate POS"
 admin.site.site_title = "Ultimate POS Admin"
