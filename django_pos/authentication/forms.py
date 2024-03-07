60e0d0b1d79db56c0bf9ec60827859030b83fb38
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-03-07 16:20:33 +0300

    Update form field attributes in authentication forms.py

--- django_pos/authentication/forms.py
+++ django_pos/authentication/forms.py
@@ -7,49 +7,38 @@ class LoginForm(forms.Form):
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                "placeholder": "Username",
-                "class": "form-control form-control-user"
+                "placeholder": "user@tomorrow.co.ke",
+                "class": "form-control form-control-user",
             }
-        ))
+        )
+    )
     password = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={
-                "placeholder": "Password",
-                "class": "form-control form-control-user"
-            }
-        ))
+            attrs={"placeholder": "Password", "class": "form-control form-control-user"}
+        )
+    )
 
 
 class SignUpForm(UserCreationForm):
     username = forms.CharField(
         widget=forms.TextInput(
-            attrs={
-                "placeholder": "Username",
-                "class": "form-control"
-            }
-        ))
+            attrs={"placeholder": "Username", "class": "form-control"}
+        )
+    )
     email = forms.EmailField(
-        widget=forms.EmailInput(
-            attrs={
-                "placeholder": "Email",
-                "class": "form-control"
-            }
-        ))
+        widget=forms.EmailInput(attrs={"placeholder": "Email", "class": "form-control"})
+    )
     password1 = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={
-                "placeholder": "Password",
-                "class": "form-control"
-            }
-        ))
+            attrs={"placeholder": "Password", "class": "form-control"}
+        )
+    )
     password2 = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={
-                "placeholder": "Password confirmation",
-                "class": "form-control"
-            }
-        ))
+            attrs={"placeholder": "Password confirmation", "class": "form-control"}
+        )
+    )
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ("username", "email", "password1", "password2")
