bd2344b7b1562d7c09ff671c953238378e752546
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-04-05 21:49:27 +0300

    Remove unused imports in customers/views.py

--- django_pos/customers/views.py
+++ django_pos/customers/views.py
@@ -1,7 +1,3 @@
-from operator import le
-import re
-import stat
-from turtle import st
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse, JsonResponse
