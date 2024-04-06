90fcef31af8012ab91a081868bd9fccaf8a56e0a
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-04-06 09:39:06 +0300

    Refactor URLs in customers/urls.py for consistency and readability

--- django_pos/customers/urls.py
+++ django_pos/customers/urls.py
@@ -5,13 +5,21 @@ from . import views
 app_name = "customers"
 urlpatterns = [
     # List customers
-    path('', views.customers_list_view, name='customers_list'),
+    path("", views.customers_list_view, name="customers_list"),
     # Add customer
-    path('add', views.customers_add_view, name='customers_add'),
+    path("add", views.customers_add_view, name="customers_add"),
     # Update customer
-    path('update/<str:customer_id>',
-         views.customers_update_view, name='customers_update'),
+    path(
+        "update/<str:customer_id>", views.customers_update_view, name="customers_update"
+    ),
     # Delete customer
-    path('delete/<str:customer_id>',
-         views.customers_delete_view, name='customers_delete'),
+    path(
+        "delete/<str:customer_id>", views.customers_delete_view, name="customers_delete"
+    ),
+    # Customer profile
+    path(
+        "profile/<str:id>",
+        views.customer_profile,
+        name="customer_profile",
+    ),
 ]
