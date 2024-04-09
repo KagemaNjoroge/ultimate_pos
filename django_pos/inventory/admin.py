8f0ae3ba3368f5d9aa776634ef041547d17f6ec5
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-04-09 19:40:10 +0300

    Refactor admin.py in inventory app

--- django_pos/inventory/admin.py
+++ django_pos/inventory/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from .models import Inventory
 
 
-# Register your models here.
-
-
 class InventoryAdmin(admin.ModelAdmin):
     list_display = ['product', 'quantity', 'date_added', 'date_modified']
 
