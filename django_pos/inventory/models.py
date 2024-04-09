4be045dee7f9fc726c1f044d93f96706dce7afb8
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-04-09 19:40:18 +0300

    Refactor models.py in inventory app

--- django_pos/inventory/models.py
+++ django_pos/inventory/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from products.models import Product
 
+
 # Create your models here.
-
-
 class Inventory(models.Model):
     product = models.OneToOneField(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
