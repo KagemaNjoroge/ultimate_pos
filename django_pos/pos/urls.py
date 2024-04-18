ff50bb3098fe3f5fc6d6a939de233fee088d04d5
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-04-18 17:01:24 +0300

    Add subscription_page and register_company views to pos/urls.py

--- django_pos/pos/urls.py
+++ django_pos/pos/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("pos/", views.pos, name="pos"),
     path("notifications/", views.get_notifications, name="notifications"),
     path("notifications/<int:id>/", views.get_notifications, name="notifications"),
+    path("subscription/", view=views.subscription_page, name="subscription_page"),
+    path("management/", view=views.register_company, name="resgister_company"),
 ]
