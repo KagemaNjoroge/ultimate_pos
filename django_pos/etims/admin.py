4f51280c56c2a8c885c8cbfa9c2a7f1c3749321b
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-04-18 16:05:38 +0300

    Refactor admin.py to disable add, change, and delete permissions for Country, Notice, TaxType, UnitOfQuantity, and PackagingUnits models

--- django_pos/etims/admin.py
+++ django_pos/etims/admin.py
@@ -4,36 +4,86 @@ from .models import Country, Notice, TaxType, UnitOfQuantity, PackagingUnits
 
 # Register your models here.
 
+
 class CountryAdmin(admin.ModelAdmin):
-    list_display = ('countryId', 'countryCode', 'countryName', 'currencyCode')
+    list_display = ("countryId", "countryCode", "countryName", "currencyCode")
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 admin.site.register(Country, CountryAdmin)
 
 
 class NoticeAdmin(admin.ModelAdmin):
-    list_display = ('noticeNumber', 'title', 'read')
+    list_display = ("noticeNumber", "title", "read")
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 admin.site.register(Notice, NoticeAdmin)
 
 
 class TaxTypeAdmin(admin.ModelAdmin):
-    list_display = ('taxTypeName', 'taxTypeIdentifier')
+    list_display = ("taxTypeName", "taxTypeIdentifier")
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 admin.site.register(TaxType, TaxTypeAdmin)
 
 
 class UnitOfQuantityAdmin(admin.ModelAdmin):
-    list_display = ('unitOfQuantityId', 'unitOfQuantityName', 'unitOfQuantityDescription')
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    list_display = (
+        "unitOfQuantityId",
+        "unitOfQuantityName",
+        "unitOfQuantityDescription",
+    )
 
 
 admin.site.register(UnitOfQuantity, UnitOfQuantityAdmin)
 
 
 class PackagingUnitsAdmin(admin.ModelAdmin):
-    list_display = ('packagingUnitId', 'packagingUnitName', 'packagingUnitDescription')
+    list_display = ("packagingUnitId", "packagingUnitName", "packagingUnitDescription")
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 admin.site.register(PackagingUnits, PackagingUnitsAdmin)
