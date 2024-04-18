a6d73e72ce33fadd2cf79f89a502bfe97be3e9a0
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-04-18 17:01:04 +0300

    Refactor admin.py to disable add_permission for SubscriptionAdmin in django_pos/company/admin.py

--- django_pos/company/admin.py
+++ django_pos/company/admin.py
@@ -19,15 +19,15 @@ class CompanyAdmin(admin.ModelAdmin):
 class SubscriptionAdmin(admin.ModelAdmin):
     # should not be able to add or change subscription details
     list_display = ("company", "start_date", "end_date", "is_active")
+    # comment out for testing
+    # def has_add_permission(self, request):
+    #     return False
 
-    def has_add_permission(self, request):
-        return False
+    # def has_change_permission(self, request, obj=None):
+    #     return False
 
-    def has_change_permission(self, request, obj=None):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
 
 
 admin.site.register(Company, CompanyAdmin)
