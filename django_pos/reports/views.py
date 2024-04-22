94bc1db9a435692e3732bb3686ef8d7a502a8de8
Jashanpreet15 <161159112+jshn7299@users.noreply.github.com>
2024-04-22 17:15:31 +0300

    Add index view to reports/urls.py

--- django_pos/reports/views.py
+++ django_pos/reports/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, "reports/index.html", context={"active_icon": "reports"})
