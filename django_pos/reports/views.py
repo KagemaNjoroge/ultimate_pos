6bf4c198359ee3575a79d5fa49dc3e5a704f5f83
Jashanpreet15 <reecejames934@gmail.com>
2024-04-26 01:49:49 +0300

    Add login_required decorator to reports index view

--- django_pos/reports/views.py
+++ django_pos/reports/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from pos.views import check_subscription
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 
+@login_required(login_url="/accounts/login/")
+@check_subscription
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "reports/index.html", context={"active_icon": "reports"})
