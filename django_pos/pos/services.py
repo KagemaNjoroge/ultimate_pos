5fc14a97ef19adb34dff807dd204b7b12dae907c
Jashanpreet15 <reecejames934@gmail.com>
2024-04-26 01:43:05 +0300

    Update timeout for license fetch request in services.py

--- django_pos/pos/services.py
+++ django_pos/pos/services.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 def refresh_license(company_id) -> dict:
     server = settings.LICENSE_SERVER
-    req = requests.get(f"{server}/licenses/fetch/{company_id}")
+    req = requests.get(f"{server}/licenses/fetch/{company_id}", timeout=5)
     try:
         data = req.json()
         # license = License.objects.get(company_id=company_id)
