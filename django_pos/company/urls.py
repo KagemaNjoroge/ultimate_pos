55f3339d5e566923dbff58d5bbea0269af1fb628
Jashanpreet15 <reecejames934@gmail.com>
2024-04-26 01:50:49 +0300

    Refactor company URLs to include named paths for better readability

--- django_pos/company/urls.py
+++ django_pos/company/urls.py
@@ -4,7 +4,7 @@ from .views import CompanyView, index
 
 app_name = "company"
 urlpatterns = [
-    path("", CompanyView.as_view()),
-    path("<int:id>/", CompanyView.as_view()),
+    path("", CompanyView.as_view(), name="company"),
+    path("<int:id>/", CompanyView.as_view(), name="company_id"),
     path("settings/", view=index, name="settings"),
 ]
