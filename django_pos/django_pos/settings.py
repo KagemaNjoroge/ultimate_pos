f8b09cbbbbac248a6ce4475e512c80241e53cd28
Jashanpreet15 <reecejames934@gmail.com>
2024-05-07 19:14:00 +0300

    chore: Update settings.py for logging configuration

--- django_pos/django_pos/settings.py
+++ django_pos/django_pos/settings.py
@@ -118,6 +118,61 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# logging
+# LOGGING = {
+#     "version": 1,
+#     "disable_existing_loggers": False,
+#     "formatters": {
+#         "verbose": {
+#             "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+#             "style": "{",
+#         },
+#         "simple": {
+#             "format": "{levelname} {message}",
+#             "style": "{",
+#         },
+#     },
+#     "filters": {
+#         "special": {
+#             "()": "project.logging.SpecialFilter",
+#             "foo": "bar",
+#         },
+#         "require_debug_true": {
+#             "()": "django.utils.log.RequireDebugTrue",
+#         },
+#     },
+#     "handlers": {
+#         "console": {
+#             "level": "INFO",
+#             "filters": ["require_debug_true"],
+#             "class": "logging.StreamHandler",
+#             "formatter": "simple",
+#         },
+#         "mail_admins": {
+#             "level": "ERROR",
+#             "class": "django.utils.log.AdminEmailHandler",
+#             "filters": ["special"],
+#         },
+#     },
+#     "loggers": {
+#         "django": {
+#             "handlers": ["console"],
+#             "propagate": True,
+#         },
+#         "django.request": {
+#             "handlers": ["mail_admins"],
+#             "level": "ERROR",
+#             "propagate": False,
+#         },
+#         "myproject.custom": {
+#             "handlers": ["console", "mail_admins"],
+#             "level": "INFO",
+#             "filters": ["special"],
+#         },
+#     },
+# }
+
+
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
