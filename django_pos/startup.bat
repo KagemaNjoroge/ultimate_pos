e6f7c16aaadb1d198346893d4c66957cddc685e8
Jashanpreet15 <reecejames934@gmail.com>
2024-05-10 15:03:40 +0300

    Add windows local server auto start scripts

---
+++ django_pos/startup.bat
@@ -0,0 +1,11 @@
+@echo off
+rem Navigate to your Django project directory
+cd /d C:\Users\kaman\Desktop\ultimate_pos\django_pos
+
+rem Activate your virtual environment if you're using one
+rem Example:
+call .venv\Scripts\activate
+
+rem Start Django development server
+rem Example:
+python manage.py runserver 0.0.0.0:8000	
---
+++ django_pos/startup.vbs
@@ -0,0 +1,3 @@
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run chr(34) & "C:\Users\kaman\Desktop\ultimate_pos\django_pos\startup.bat" & Chr(34), 0
+Set WshShell = Nothing
