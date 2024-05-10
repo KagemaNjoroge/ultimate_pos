57e50055b02b1c279c9597ef3ee07ac51f918937
Kagema Njoroge <reecejames934@gmail.com>
2024-05-10 18:59:02 +0300

    feat: Add shopping cart functionality
    This commit adds the necessary JavaScript code to implement a shopping cart feature. The code includes functions for adding items to the cart, setting the count for an item, removing items from the cart, removing all items from the cart, and calculating the total count and total price of the cart. The shopping cart functionality is encapsulated in the `shoppingCart` object, which is defined as an immediately-invoked function expression (IIFE). This implementation will allow users to easily manage their shopping cart and perform various operations on it.
    
    Note: This commit message is based on the code changes and does not take into account the recent user commits or repository commits.

---
+++ django_pos/static/utils/cart.js
@@ -0,0 +1,173 @@
+function togglePlaySound() {
+  // session storage is used to store the state of the sound - under AppSettings Session
+  let settings = sessionStorage.getItem("AppSettings");
+  let sound = settings ? JSON.parse(settings).play_sound : true;
+  // toggle sound state
+  sound = !sound;
+  sessionStorage.setItem("AppSettings", JSON.stringify({ play_sound: sound }));
+
+  return sound;
+}
+
+$(document).ready(function () {
+  $("#sound_btn").on("click", function () {
+    let sound = togglePlaySound();
+    if (sound) {
+      $(this).html(
+        '<i class="fa fa-volume-up hover-q text-muted" title=""></i>'
+      );
+    } else {
+      $(this).html(
+        '<i class="fa fa-volume-mute hover-q text-muted" title=""></i>'
+      );
+    }
+  });
+});
+let csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+
+  return cookieValue;
+}
+function csrfSafeMethod(method) {
+  // these HTTP methods do not require CSRF protection
+  return /^(GET|HEAD|OPTIONS|TRACE)$/.test(method);
+}
+
+// To avoid error 403 Forbidden
+$.ajaxSetup({
+  beforeSend: function (xhr, settings) {
+    if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+      xhr.setRequestHeader("X-CSRFToken", getCookie("csrftoken"));
+    }
+  },
+});
+
+let shoppingCart = (function () {
+  let cart = [];
+
+  // Constructor
+  function Item(name, price, count, id) {
+    this.name = name;
+    this.price = price;
+    this.count = count;
+    this.id = id;
+  }
+
+  // Save cart
+  function saveCart() {
+    sessionStorage.setItem("shoppingCart", JSON.stringify(cart));
+  }
+
+  // Load cart
+  function loadCart() {
+    cart = JSON.parse(sessionStorage.getItem("shoppingCart"));
+  }
+  if (sessionStorage.getItem("shoppingCart") != null) {
+    loadCart();
+  }
+
+  let obj = {};
+
+  // Add to cart
+  obj.addItemToCart = function (name, price, count, id) {
+    for (let item in cart) {
+      if (cart[item].name === name) {
+        cart[item].count++;
+        saveCart();
+        return;
+      }
+    }
+    let item = new Item(name, price, count, id);
+    cart.push(item);
+    saveCart();
+  };
+  // Set count from item
+  obj.setCountForItem = function (name, count) {
+    for (let i in cart) {
+      if (cart[i].name === name) {
+        cart[i].count = count;
+        break;
+      }
+    }
+    saveCart();
+  };
+  // Remove item from cart
+  obj.removeItemFromCart = function (name) {
+    for (let item in cart) {
+      if (cart[item].name === name) {
+        cart[item].count--;
+        if (cart[item].count === 0) {
+          cart.splice(item, 1);
+        }
+        break;
+      }
+    }
+    saveCart();
+  };
+
+  // Remove all items from cart
+  obj.removeItemFromCartAll = function (name) {
+    for (let item in cart) {
+      if (cart[item].name === name) {
+        cart.splice(item, 1);
+        break;
+      }
+    }
+    saveCart();
+  };
+
+  // Clear cart
+  obj.clearCart = function () {
+    cart = [];
+    saveCart();
+  };
+
+  // Count cart
+  obj.totalCount = function () {
+    let totalCount = 0;
+    for (let item in cart) {
+      totalCount += cart[item].count;
+    }
+    return totalCount;
+  };
+
+  // Total cart
+  obj.totalCart = function () {
+    let totalCart = 0;
+    for (let item in cart) {
+      totalCart += cart[item].price * cart[item].count;
+    }
+    return Number(totalCart.toFixed(2));
+  };
+
+  // List cart
+  obj.listCart = function () {
+    let products = [];
+    for (let i in JSON.parse(sessionStorage.getItem("shoppingCart"))) {
+      let item = cart[i];
+      let product = {
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        count: item.count,
+        total: (item.price * item.count).toFixed(2),
+      };
+      products.push(product);
+    }
+    return products;
+  };
+
+  return obj;
+})();
