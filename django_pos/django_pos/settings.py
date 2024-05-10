6c82d61d3dfe1387e719a469156767f5f6f6d05f
Kagema Njoroge <reecejames934@gmail.com>
2024-05-10 18:35:52 +0300

    chore: Remove commented out code for test database configuration

--- django_pos/django_pos/settings.py
+++ django_pos/django_pos/settings.py
@@ -96,15 +96,6 @@ DATABASES = {
 }
 
 
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
