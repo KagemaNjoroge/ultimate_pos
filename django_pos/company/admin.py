2db370dbdb3f681c6eb262751b13eee4d5212e39
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-11 22:20:02 +0300

    refactor: Remove invoice_template_id and bill_template_id from Company model

--- django_pos/company/admin.py
+++ django_pos/company/admin.py
@@ -1,33 +1,61 @@
 from django.contrib import admin
-from .models import Company, Subscription
+from .models import Company, Subscription, Branch
+
+
+class BranchAdmin(admin.ModelAdmin):
+    list_display = (
+        "branch_name",
+        "phone_number",
+        "email",
+        "city",
+        "address",
+        "branch_id",
+    )
+    # should not be able to add or change branch details
+
+    # def has_add_permission(self, request):
+    #     return False
+
+    # def has_change_permission(self, request, obj=None):
+    #     return False
+
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
+
+    # def has_module_permission(self, request):
+    #     return False
+
+
+admin.site.register(Branch, BranchAdmin)
 
 
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ("company_name", "phone_number", "currency_symbol")
     # should not be able to add or change company details
 
-    # def has_add_permission(self, request):
-    #     return False
+    def has_add_permission(self, request):
+        return False
 
-    # def has_change_permission(self, request, obj=None):
-    #     return False
+    def has_change_permission(self, request, obj=None):
+        return False
 
-    # def has_delete_permission(self, request, obj=None):
-    #     return False
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 class SubscriptionAdmin(admin.ModelAdmin):
     # should not be able to add or change subscription details
     list_display = ("company", "start_date", "end_date", "is_active")
+
     # comment out for testing
-    # def has_add_permission(self, request):
-    #     return False
+    def has_add_permission(self, request):
+        return False
 
-    # def has_change_permission(self, request, obj=None):
-    #     return False
+    def has_change_permission(self, request, obj=None):
+        return False
 
-    # def has_delete_permission(self, request, obj=None):
-    #     return False
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 admin.site.register(Company, CompanyAdmin)
