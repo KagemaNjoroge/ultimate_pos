755084615d5621bdf97df0cc3cfa7fa8ccd9e1f7
Kagema Njoroge <reecejames934@gmail.com>
2024-05-11 14:08:08 +0300

    feat: Add duration sales report route
    This commit adds a new route for generating a duration sales report in the `reports` app. The route is defined as "/duration-sales-report/" and is mapped to the `duration_sales_report` view function. This addition allows users to generate and view sales reports for a specific duration, providing valuable insights into the sales performance of the system.

--- django_pos/reports/urls.py
+++ django_pos/reports/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, duration_sales_report
 
 app_name = "reports"
 urlpatterns = [
     path("", index, name="index"),
+    path("duration-sales-report/", duration_sales_report, name="duration_sales_report"),
 ]
