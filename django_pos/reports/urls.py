d11bfcf9844864ad8690ea34cd9689922ba25363
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-12 13:16:47 +0300

    feat: Add sales-related endpoints for reports

--- django_pos/reports/urls.py
+++ django_pos/reports/urls.py
@@ -1,8 +1,23 @@
 from django.urls import path
-from .views import index, duration_sales_report
+from .views import (
+    index,
+    duration_sales_report,
+    sales_this_month,
+    sales_this_week,
+    best_selling_product,
+    get_best_selling_category,
+)
 
 app_name = "reports"
 urlpatterns = [
     path("", index, name="index"),
     path("duration-sales-report/", duration_sales_report, name="duration_sales_report"),
+    path("sales-this-month/", sales_this_month, name="sales_this_month"),
+    path("sales-this-week/", sales_this_week, name="sales_this_week"),
+    path("best-selling-product/", best_selling_product, name="best_selling_product"),
+    path(
+        "best-selling-category/",
+        get_best_selling_category,
+        name="best_selling_category",
+    ),
 ]
