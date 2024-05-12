44bec0b0f7f04e473e506a297a4ecdd3b3110ffa
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-12 13:16:23 +0300

    feat: Add checkout and fetch_packages endpoints
    This commit adds the "checkout" and "fetch_packages" endpoints to the `django_pos/pos/urls.py` file. The "checkout" endpoint allows users to make payments by specifying the amount, while the "fetch_packages" endpoint retrieves package data. These additions enhance the functionality of the application by providing payment processing and package retrieval capabilities.

--- django_pos/pos/urls.py
+++ django_pos/pos/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path("notifications/<int:id>/", views.get_notifications, name="notifications"),
     path("subscription/", view=views.subscription_page, name="subscription_page"),
     path("management/", view=views.register_company, name="register_company"),
+    path("checkout/<int:amount>/", view=views.checkout, name="checkout"),
+    path("packages/", views.fetch_packages, name="fetch_packages"),
 ]
