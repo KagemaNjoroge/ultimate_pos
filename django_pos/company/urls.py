19ca269c87b06f10e38a0f259dc26539f95458b6
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-13 16:08:09 +0300

    refactor: Update company URLs and views
    This commit updates the company URLs and views in the django_pos/company/urls.py file. It replaces the "test/" URL pattern with "branches/" to align with the project's URL structure. This change improves the readability and consistency of the URLs.

--- django_pos/company/urls.py
+++ django_pos/company/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import CompanyView, index, test_firebase
+from .views import CompanyView, index, branches
 
 app_name = "company"
 urlpatterns = [
     path("", CompanyView.as_view(), name="company"),
     path("<int:id>/", CompanyView.as_view(), name="company_id"),
     path("settings/", view=index, name="settings"),
-    path("test/", view=test_firebase, name="test"),
+    path("branches/", view=branches, name="branches"),
 ]
