f9879a5b37a024e869848343317064fd0a46c865
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-13 16:11:07 +0300

    feat: Add Supplier model and migration
    This commit adds the Supplier model and migration to the django_pos/suppliers app. The model includes fields for name, address, phone, email, KRA PIN, website, and logo. The migration file is created to apply the changes to the database schema. This change allows for the management of suppliers in the application.

--- django_pos/suppliers/models.py
+++ django_pos/suppliers/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    kra_pin = models.CharField(max_length=11, blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    logo = models.ImageField(upload_to="suppliers/", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def to_json(self):
+        return {
+            "name": self.name,
+            "address": self.address,
+            "phone": self.phone,
+            "email": self.email,
+            "kra_pin": self.kra_pin,
+            "website": self.website,
+            "logo": self.logo.url if self.logo else "",
+        }
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Supplier"
+        verbose_name_plural = "Suppliers"
+        db_table = "Suppliers"
