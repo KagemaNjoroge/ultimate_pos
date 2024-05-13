35e4867ba6be60b2718ca0210e00b8da6a0daade
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-13 19:53:09 +0300

    feat: Update gunicorn configuration
    This commit updates the gunicorn configuration in the `views.py` file. It adds a systemd unit file for gunicorn, specifying the user, group, working directory, and execution command. This change ensures that gunicorn is properly configured and can be managed as a systemd service.

--- django_pos/accounting/views.py
+++ django_pos/accounting/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
 
+import django_pos
+
 # Create your views here.
 
 
 def index(request):
     return render(request, "accounting/index.html")
+
+
+
+
+
+
+# [Unit]
+# Description=gunicorn daemon
+# Requires=gunicorn.socket
+# After=network.target
+
+# [Service]
+# User=Njoroge
+# Group=www-data
+# WorkingDirectory=/home/Njoroge/ultimate_pos/django_pos
+# ExecStart=/home/Njoroge/ultimate_pos/django_pos/venv/bin/gunicorn \
+#           --access-logfile - \
+#           --workers 3 \
+#           --bind unix:/run/gunicorn.sock \
+#           django_pos.wsgi:application
+
+# [Install]
+# WantedBy=multi-user.target
