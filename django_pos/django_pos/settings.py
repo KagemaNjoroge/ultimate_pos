6c6731d2a1ada036b99d9013494dc7d169665bac
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-13 19:52:55 +0300

    feat: Update static files configuration

--- django_pos/django_pos/settings.py
+++ django_pos/django_pos/settings.py
@@ -122,8 +122,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 STATIC_URL = "/static/"
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Media Files
 MEDIA_URL = "/media/"
