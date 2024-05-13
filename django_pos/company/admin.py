8d2ace2756a6b5759552487c303214c4705b2db0
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-13 22:19:33 +0300

    chore: enable editing of compant and branch details in admin page

--- django_pos/company/admin.py
+++ django_pos/company/admin.py
@@ -33,14 +33,14 @@ class CompanyAdmin(admin.ModelAdmin):
     list_display = ("company_name", "phone_number", "currency_symbol")
     # should not be able to add or change company details
 
-    def has_add_permission(self, request):
-        return False
+    # def has_add_permission(self, request):
+    #     return False
 
-    def has_change_permission(self, request, obj=None):
-        return False
+    # def has_change_permission(self, request, obj=None):
+    #     return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
 
 
 class SubscriptionAdmin(admin.ModelAdmin):
@@ -48,14 +48,14 @@ class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ("company", "start_date", "end_date", "is_active")
 
     # comment out for testing
-    def has_add_permission(self, request):
-        return False
+    # def has_add_permission(self, request):
+    #     return False
 
-    def has_change_permission(self, request, obj=None):
-        return False
+    # def has_change_permission(self, request, obj=None):
+    #     return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
 
 
 admin.site.register(Company, CompanyAdmin)
