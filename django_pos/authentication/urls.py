616a2e1a41faffd2afbbbac5dd1326263b761cef
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-13 16:07:40 +0300

    ```text refactor: Update authentication URLs and views
    This commit updates the authentication URLs and views in the django_pos/authentication/urls.py file. It adds a new URL pattern for the users index page and updates the existing URL patterns to remove the "accounts/" prefix. This change aligns the authentication URLs with the project's URL structure and improves the readability of the URLs.

--- django_pos/authentication/urls.py
+++ django_pos/authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import login_view, register_user, logout_view, profile
+from .views import login_view, register_user, logout_view, profile, index
 
 app_name = "authentication"
 urlpatterns = [
-    path('accounts/login/', login_view, name="login"),
-    path('accounts/register/', register_user, name="register"),
-    path('accounts/logout/', logout_view, name="logout"),
-    path('accounts/profile/', profile, name='profile')
+    path("login/", login_view, name="login"),
+    path("register/", register_user, name="register"),
+    path("logout/", logout_view, name="logout"),
+    path("profile/", profile, name="profile"),
+    path("", index, name="users_index"),
 ]
