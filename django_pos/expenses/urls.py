1360eba5aa54e01b4f3154962fdbc9c7b71feff7
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-14 21:40:34 +0300

    feat: Add expenses_operations and category routes
    This commit adds the expenses_operations route to handle requests for specific expenses by their ID. It also adds the category route to handle requests for expense categories. These routes enhance the functionality of the expenses application by providing endpoints for managing expenses and expense categories.

--- django_pos/expenses/urls.py
+++ django_pos/expenses/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "expenses"
 
 urlpatterns = [
+    path("<int:id>", views.index, name="expenses_operations"),
     path("", views.index, name="index"),
+    path("category", views.expense_categories, name="category"),
 ]
