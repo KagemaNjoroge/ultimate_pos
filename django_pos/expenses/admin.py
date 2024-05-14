1acf9c96a3611e8b998983005f58b78e57039b94
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-14 21:40:12 +0300

    feat: Update admin.py to register Expense and ExpenseCategory models
    This commit updates the `admin.py` file to register the Expense and ExpenseCategory models. It adds the ExpenseAdmin and ExpenseCategoryAdmin classes with customized display, search, filter, ordering, and pagination options. This change improves the administration interface for managing expenses and expense categories in the application.

--- django_pos/expenses/admin.py
+++ django_pos/expenses/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Expense, ExpenseCategory
 
 # Register your models here.
+
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ("expense_name", "category", "amount", "date_added", "date_modified")
+    search_fields = ("expense_name", "category__category_name", "amount")
+    list_filter = ("category", "date_added", "date_modified")
+    date_hierarchy = "date_added"
+    ordering = ("-date_added",)
+    list_per_page = 10
+
+
+class ExpenseCategoryAdmin(admin.ModelAdmin):
+    list_display = ("category_name", "category_description", "is_recurring")
+    search_fields = ("category_name", "category_description")
+    list_filter = ("is_recurring",)
+    ordering = ("category_name",)
+    list_per_page = 10
+
+
+admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(ExpenseCategory, ExpenseCategoryAdmin)
