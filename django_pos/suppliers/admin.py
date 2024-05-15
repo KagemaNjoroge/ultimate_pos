1de624f13123f35379634e419d0da1aa295e057a
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-15 13:27:14 +0300

    refactor: Update SupplierAdmin in admin.py
    This commit updates the SupplierAdmin class in the admin.py file. It adds additional fields to the list_display, search_fields, and list_filter attributes to improve the visibility and filtering options for suppliers in the admin interface. The list_per_page attribute is also set to 10 for better pagination. This change enhances the usability and functionality of the admin panel for managing suppliers.

--- django_pos/suppliers/admin.py
+++ django_pos/suppliers/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Supplier
 
 # Register your models here.
+
+
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "address",
+        "phone",
+        "email",
+        "kra_pin",
+        "website",
+        "logo",
+        "branch",
+    ]
+    search_fields = [
+        "name",
+        "address",
+        "phone",
+        "email",
+        "kra_pin",
+        "website",
+        "logo",
+        "branch",
+    ]
+    list_filter = [
+        "name",
+        "address",
+        "phone",
+        "email",
+        "kra_pin",
+        "website",
+        "logo",
+        "branch",
+    ]
+    list_per_page = 10
+
+
+admin.site.register(Supplier, SupplierAdmin)
