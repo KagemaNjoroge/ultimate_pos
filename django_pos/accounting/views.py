04afe3c04b847db0bef8f6ae1e7c8eeabfaf46d4
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-18 23:46:49 +0300

    refactor: Add login_required decorator to index view
    This commit adds the `@login_required` decorator to the `index` view in the `accounting/views.py` file. The decorator ensures that only authenticated users can access the index page by redirecting them to the login page if they are not logged in. This change enhances the security of the application by restricting unauthorized access to sensitive information.

--- django_pos/accounting/views.py
+++ django_pos/accounting/views.py
@@ -1,32 +1,9 @@
 from django.shortcuts import render
-
-import django_pos
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required(login_url="/users/login/")
 def index(request):
     return render(request, "accounting/index.html")
-
-
-
-
-
-
-# [Unit]
-# Description=gunicorn daemon
-# Requires=gunicorn.socket
-# After=network.target
-
-# [Service]
-# User=Njoroge
-# Group=www-data
-# WorkingDirectory=/home/Njoroge/ultimate_pos/django_pos
-# ExecStart=/home/Njoroge/ultimate_pos/django_pos/venv/bin/gunicorn \
-#           --access-logfile - \
-#           --workers 3 \
-#           --bind unix:/run/gunicorn.sock \
-#           django_pos.wsgi:application
-
-# [Install]
-# WantedBy=multi-user.target
