ce9464392c343602a04497188359bda8e2efb908
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-18 23:48:09 +0300

    refactor: Add login_required decorator to etims view

--- django_pos/etims/views.py
+++ django_pos/etims/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url="/users/login/")
 def etims(request: HttpRequest) -> HttpResponse:
     return render(request, "etims/etims.html", {"active_icon": "etims"})
