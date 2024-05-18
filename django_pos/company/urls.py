66b54b524cb6d163ad470844188e062720184201
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-18 23:47:11 +0300

    feat: Add invoice design view
    This commit adds a new view, `invoice_design`, to the `company/views.py` file. The view is responsible for rendering the invoice design page, allowing users to customize the design of their invoices. This change enhances the functionality of the application by providing users with the ability to personalize their invoice templates.

--- django_pos/company/urls.py
+++ django_pos/company/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import CompanyView, index, branches
+from .views import CompanyView, index, branches, invoice_design
 
 app_name = "company"
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path("<int:id>/", CompanyView.as_view(), name="company_id"),
     path("settings/", view=index, name="settings"),
     path("branches/", view=branches, name="branches"),
+    path("invoice-design/", view=invoice_design, name="invoice_design"),
 ]
