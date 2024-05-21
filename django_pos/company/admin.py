4735aeec4b9e248d26e9b6064f74846e8740f4a1
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-21 13:48:46 +0300

    chore: Update admin permissions for Company and Subscription models
    This commit updates the `has_add_permission` and `has_delete_permission` methods in the `CompanyAdmin` and `SubscriptionAdmin` classes in the `admin.py` file. The changes ensure that only one company can be created and that the delete permission is disabled for both models. This improves the security and integrity of the application's data.

--- django_pos/company/admin.py
+++ django_pos/company/admin.py
@@ -33,14 +33,16 @@ class CompanyAdmin(admin.ModelAdmin):
     list_display = ("company_name", "phone_number", "currency_symbol")
     # should not be able to add or change company details
 
-    # def has_add_permission(self, request):
-    #     return False
+    def has_add_permission(self, request):
+        # only one company should be created
+        if Company.objects.count() == 0:
+            return True
 
     # def has_change_permission(self, request, obj=None):
     #     return False
 
-    # def has_delete_permission(self, request, obj=None):
-    #     return False
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 class SubscriptionAdmin(admin.ModelAdmin):
@@ -48,14 +50,14 @@ class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ("company", "start_date", "end_date", "is_active")
 
     # comment out for testing
-    # def has_add_permission(self, request):
-    #     return False
+    def has_add_permission(self, request):
+        return False
 
-    # def has_change_permission(self, request, obj=None):
-    #     return False
+    def has_change_permission(self, request, obj=None):
+        return False
 
-    # def has_delete_permission(self, request, obj=None):
-    #     return False
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 admin.site.register(Company, CompanyAdmin)
