e26e64d9dccd278c47c99f27cd1aecd6c0557b2a
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-28 11:58:44 +0300

    feat: Add Purchase model for managing purchases
    This commit adds the `Purchase` model to the `models.py` file in the `purchases` app. The model includes fields for the supplier, product, status, date created, and date modified. This change enhances the functionality of the application by providing a way to manage purchases.

--- django_pos/purchases/models.py
+++ django_pos/purchases/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from products.models import Product
+from suppliers.models import Supplier
 
 # Create your models here.
+
+
+class Purchase(models.Model):
+    purchase_status = (
+        ("PENDING", "Pending"),
+        ("DONE", "Done"),
+    )
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    status = models.CharField(
+        choices=purchase_status, max_length=30, verbose_name="Status of the purchase"
+    )
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    # TODO: to add more fields
+    def __str__(self) -> str:
+        return self.id
+
+    class Meta:
+        db_table = "Purchases"
+        verbose_name_plural = "Purchases"
+        verbose_name = "Purchase"
