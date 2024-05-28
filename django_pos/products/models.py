61da3c61bbc0f6b59f18c2c85fb272567810d257
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-28 11:58:35 +0300

    chore: Refactor Category and Product models
    This commit refactors the `Category` and `Product` models in the `models.py` file. The changes include removing unused imports, simplifying the `to_json` method, and removing commented-out code. This improves code readability and maintainability.

--- django_pos/products/models.py
+++ django_pos/products/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.forms import model_to_dict
 
 
 class Category(models.Model):
@@ -18,19 +17,17 @@ class Category(models.Model):
         db_table = "Category"
         verbose_name_plural = "Categories"
         verbose_name = "Category"
-        # permissions
-        permissions = [
-            ("can_view_category", "Can view category"),
-            ("can_add_category", "Can add category"),
-            ("can_edit_category", "Can edit category"),
-            ("can_delete_category", "Can delete category"),
-        ]
 
     def __str__(self) -> str:
         return self.name
 
     def to_json(self) -> dict:
-        return model_to_dict(self)
+        return {
+            "name": self.name,
+            "status": self.status,
+            "description": self.description,
+            "id": self.id,
+        }
 
 
 class Product(models.Model):
@@ -54,35 +51,22 @@ class Product(models.Model):
 
     price = models.FloatField(default=0)
 
-    # def save(self, *args, **kwargs):
-    #     # if track inventory, create a new inventory record
-    #     if self.track_inventory:
-    #         Inventory.objects.create(product=self, quantity=0)
-
     class Meta:
         # Table's name
         db_table = "Product"
         verbose_name_plural = "Products"
         verbose_name = "Product"
-        # permissions
-        permissions = [
-            ("can_view_product", "Can view product"),
-            ("can_add_product", "Can add product"),
-            ("can_edit_product", "Can edit product"),
-            ("can_delete_product", "Can delete product"),
-        ]
 
     def __str__(self) -> str:
         return self.name
 
     def to_json(self):
-        item = model_to_dict(self)
+        item = {}
         item["id"] = self.id
         item["text"] = self.name
         item["category"] = self.category.name
         item["quantity"] = 1
         item["total_product"] = 0
-        # if image exists, then return the image url, else return empty string
-        item["image"] = self.image.url if self.image else ""
+        item["image"] = self.image.url if self.image else None
         item["track_inventory"] = self.track_inventory
         return item
