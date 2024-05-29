980151acbac7a1471d4cb6218fa1e20dd85f1fe0
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-05-29 14:09:40 +0300

    feat: Update ALLOWED_HOSTS and add email settings
    The commit updates the ALLOWED_HOSTS configuration in the settings.py file to include additional hostnames for the application. It also adds email settings for SMTP configuration, including the host, port, TLS usage, username, password, and default from email address. This change enhances the security and functionality of the application by allowing it to be accessed from multiple hostnames and enabling email notifications.

--- django_pos/django_pos/settings.py
+++ django_pos/django_pos/settings.py
@@ -13,7 +13,13 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "http://localhost",
+    "http://pos.tomorrow.co.ke",
+    "https://pos.tomorrow.co.ke",
+    "https://localhost",
+    "localhost",
+]
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost",
     "http://pos.tomorrow.co.ke",
@@ -144,8 +150,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
-# configuration for the license server
-LICENSE_SERVER = "https://tomorrow.co.ke"
-MY_COMPANY_NAME = "Tomorrow Solutions"
-MY_COMPANY_ID = "1"
+# Email Settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
