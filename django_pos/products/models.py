a03b1a2faa69d34c56751ea7106cd478f8f1af1b
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-06-11 20:55:04 +0300

    feat: Add fields for country of origin, packaging unit, product type, and quantity unit to Product model
    This commit adds new fields to the Product model in the `products` app. The fields include `country_of_origin`, `packaging_unit`, `product_type`, and `quantity_unit`. These changes enhance the functionality of the application by providing more detailed information about the products and improving the management of inventory.

--- django_pos/products/models.py
+++ django_pos/products/models.py
@@ -32,6 +32,14 @@ class Category(models.Model):
 
 class Product(models.Model):
     STATUS_CHOICES = (("ACTIVE", "Active"), ("INACTIVE", "Inactive"))  # new
+    PRODUCT_TYPES = (
+        ("1", "Raw Material"),
+        ("2", "Finished Product"),
+        (
+            "3",
+            "Service without stock",
+        ),
+    )
 
     name = models.CharField(max_length=256)
     description = models.TextField(max_length=256)
@@ -41,6 +49,8 @@ class Product(models.Model):
         choices=STATUS_CHOICES,
         max_length=100,
         verbose_name="Status of the product",
+        blank=True,
+        null=True,
     )
     category = models.ForeignKey(
         Category,
@@ -48,8 +58,18 @@ class Product(models.Model):
         on_delete=models.CASCADE,
         db_column="category",
     )
-
     price = models.FloatField(default=0)
+
+    # etims related
+    country_of_origin = models.CharField(max_length=5, blank=True, null=True)
+    product_type = models.CharField(
+        max_length=30,
+        choices=PRODUCT_TYPES,
+        verbose_name="Product Type",
+        default="Finished Product",
+    )
+    packaging_unit = models.CharField(max_length=20, blank=True, null=True)
+    quantity_unit = models.CharField(max_length=20, blank=True, null=True)
 
     class Meta:
         # Table's name
@@ -69,4 +89,11 @@ class Product(models.Model):
         item["total_product"] = 0
         item["image"] = self.image.url if self.image else None
         item["track_inventory"] = self.track_inventory
+        item["product_type"] = (self.product_type,)
+        item["packaging_unit"] = (self.packaging_unit,)
+        item["quantity_unit"] = (self.quantity_unit,)
+        item["country_of_origin"] = self.country_of_origin
         return item
+
+    def get_product_code(self) -> str:
+        return f"{self.country_of_origin}{self.product_type}{self.packaging_unit}{self.quantity_unit}"
