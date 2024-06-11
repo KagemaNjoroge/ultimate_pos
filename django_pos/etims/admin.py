6eef539b16ea05cd060d4a8031fd9fb284f85849
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-06-11 20:49:54 +0300

    feat: Register EtimsBranch model in admin.py
    This commit registers the EtimsBranch model in the admin.py file of the etims app. By using the @admin.register decorator, this model is now displayed in the Django admin interface, allowing for easy management and administration. This change enhances the functionality of the application by providing a user-friendly interface for managing EtimsBranch objects.

--- django_pos/etims/admin.py
+++ django_pos/etims/admin.py
@@ -1,13 +1,34 @@
 from django.contrib import admin
-from .models import Country, Notice, TaxType, UnitOfQuantity, PackagingUnits
+from .models import (
+    Country,
+    EtimsNotice,
+    TaxType,
+    UnitOfQuantity,
+    PackagingUnits,
+    EtimsBranch,
+    ItemClassCodes,
+)
 
 
-# Register your models here.
+@admin.register(ItemClassCodes)
+class ItemClassCodesAdmin(admin.ModelAdmin):
+    list_display = ("itemClassCode", "itemClassCodeName")
+    search_fields = ("itemClassCode", "itemClassCodeName")
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
     list_display = ("countryId", "countryCode", "countryName", "currencyCode")
+    search_fields = ("countryName", "countryCode", "currencyCode")
 
     def has_add_permission(self, request):
         return False
@@ -19,23 +40,29 @@ class CountryAdmin(admin.ModelAdmin):
         return False
 
 
-@admin.register(Notice)
+@admin.register(EtimsNotice)
 class NoticeAdmin(admin.ModelAdmin):
     list_display = ("noticeNumber", "title", "read")
+    search_fields = ("title", "content")
+    actions = ["mark_as_read"]
 
     def has_add_permission(self, request):
         return False
 
     def has_change_permission(self, request, obj=None):
-        return False
+        return True
 
     def has_delete_permission(self, request, obj=None):
         return False
+
+    @admin.action(description="Mark as read")
+    def mark_as_read(self, request, queryset):
+        queryset.update(read=True)
 
 
 @admin.register(TaxType)
 class TaxTypeAdmin(admin.ModelAdmin):
-    list_display = ("taxTypeName", "taxTypeIdentifier")
+    list_display = ("taxTypeName",)
 
     def has_add_permission(self, request):
         return False
@@ -68,6 +95,41 @@ class UnitOfQuantityAdmin(admin.ModelAdmin):
 @admin.register(PackagingUnits)
 class PackagingUnitsAdmin(admin.ModelAdmin):
     list_display = ("packagingUnitId", "packagingUnitName", "packagingUnitDescription")
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(EtimsBranch)
+class EtimsBranchAdmin(admin.ModelAdmin):
+    list_display = (
+        "branch_tin",
+        "branch_id",
+        "branch_name",
+        "branch_status_code",
+        "province_name",
+        "district_name",
+        "sctr_name",
+        "location_description",
+        "manager_name",
+        "manager_telephone",
+        "manager_email",
+    )
+    search_fields = (
+        "branch_tin",
+        "branch_id",
+        "branch_name",
+        "province_name",
+        "district_name",
+        "sctr_name",
+        "manager_name",
+    )
 
     def has_add_permission(self, request):
         return False
