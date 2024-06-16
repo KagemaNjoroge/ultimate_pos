a89e91e2fe18e90bf1428a31a4aa559940d2d68d
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-06-16 09:46:57 +0300

    Add swagger api docs endpoint

--- django_pos/django_pos/urls.py
+++ django_pos/django_pos/urls.py
@@ -1,8 +1,23 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
 from django_pos import settings
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Ultimate POS API",
+        default_version="v1",
+        description="Ultimate POS API Documentation",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="nyamburanjorogejames@students.uonbi.ac.ke"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -36,5 +51,11 @@ urlpatterns = [
     path("expenses/", include("expenses.urls")),
     # purchases
     path("purchases/", include("purchases.urls")),
+    # Swagger
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
