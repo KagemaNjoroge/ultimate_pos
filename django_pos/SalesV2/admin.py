2ca1cce79b16112a95e273964e6a0528905478e4
Kagema Njoroge <reecejames934@gmail.com>
2024-06-19 17:50:45 +0300

    feat: Update Sale and SaleItem admin views
    This commit updates the admin views for the Sale and SaleItem models in the SalesV2 app. The SaleAdmin class now includes additional fields in the list_display attribute to provide more information about each sale. The SaleItemAdmin class also includes the "quantity" field in the list_display attribute. These changes improve the readability and usability of the admin interface for managing sales.

--- django_pos/SalesV2/admin.py
+++ django_pos/SalesV2/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Sale, SaleItem
 
-# Register your models here.
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "date_added",
+        "customer",
+        "sub_total",
+        "discount",
+        "grand_total",
+        "total_tax_amount",
+        "amount_paid",
+        "amount_change",
+        "receipt_is_printed",
+    )
+    list_filter = ("date_added", "customer", "receipt_is_printed")
+    search_fields = ("id", "customer__first_name", "customer__last_name")
+    ordering = ("date_added",)
+    date_hierarchy = "date_added"
+
+
+@admin.register(SaleItem)
+class SaleItemAdmin(admin.ModelAdmin):
+    list_display = ("product", "quantity")
+    list_filter = ("product",)
+    search_fields = ("product__name",)
+    ordering = ("product",)
