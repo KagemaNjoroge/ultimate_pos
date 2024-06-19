dbe0e28404a7ef54b9e47e79357a18af09d7e863
Kagema Njoroge <reecejames934@gmail.com>
2024-06-19 17:50:07 +0300

    feat: Add Sale and SaleItem viewsets for REST API
    This commit adds the SaleViewSet and SaleItemViewSet classes to the views.py file in the SalesV2 app. These viewsets are used to handle the REST API endpoints related to sales. The SaleViewSet is responsible for CRUD operations on the Sale model, and the SaleItemViewSet is responsible for CRUD operations on the SaleItem model. These viewsets will be used in conjunction with the SaleSerializer and SaleItemSerializer to provide the necessary functionality for managing sales through the API.

--- django_pos/SalesV2/views.py
+++ django_pos/SalesV2/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import SaleSerializer, SaleItemSerializer
+from .models import Sale, SaleItem
 
-# Create your views here.
+
+class SaleItemViewSet(viewsets.ModelViewSet):
+    queryset = SaleItem.objects.all()
+    serializer_class = SaleItemSerializer
+
+
+class SaleViewSet(viewsets.ModelViewSet):
+    queryset = Sale.objects.all()
+    serializer_class = SaleSerializer
