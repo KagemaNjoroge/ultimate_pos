09f262ecebfb364dd323fa08acb7e19ae62c9c32
Kagema Njoroge <reecejames934@gmail.com>
2024-06-19 17:43:58 +0300

    chore: Add ETIMS API endpoints for branches, notices, item class codes, and units of quantity

--- django_pos/api/urls.py
+++ django_pos/api/urls.py
@@ -1,11 +1,23 @@
 from rest_framework.routers import DefaultRouter
-
+from .vie.etims import (
+    EtimsBranchViewSet,
+    EtimsNoticeViewSet,
+    ItemClassCodesViewSet,
+    UnitsOfQuantityViewSet,
+)
 from .vie.customers import CustomersViewSet
 from .vie.expenses import ExpensesViewSet, ExpenseCategoryViewSet
 
-app_name = 'api'
+app_name = "api"
 router = DefaultRouter()
 router.register(r"expenses", ExpensesViewSet, basename="expenses")
 router.register("expense-category", ExpenseCategoryViewSet, basename="expense_category")
 router.register("customers", CustomersViewSet, basename="customers")
+router.register("etims-branch", EtimsBranchViewSet, basename="etims_branch")
+router.register("etims-notice", EtimsNoticeViewSet, basename="etims_notice")
+router.register("item-class-codes", ItemClassCodesViewSet, basename="item_class_codes")
+router.register(
+    "units-of-quantity", UnitsOfQuantityViewSet, basename="units_of_quantity"
+)
+
 urlpatterns = router.urls
