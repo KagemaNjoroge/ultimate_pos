1116abd63b493ee7fd22bd41766df6cf6a569fc3
Kagema Njoroge <reecejames934@gmail.com>
2024-06-23 17:50:25 +0300

    feat: Remove License model from models.py
    This commit removes the License model from models.py as it is no longer needed in the project. The License model was previously used for managing licenses, but it is no longer relevant to the current requirements. This change simplifies the codebase and improves maintainability.

--- django_pos/pos/models.py
+++ django_pos/pos/models.py
@@ -18,26 +18,3 @@ class Notifications(models.Model):
         verbose_name_plural = "Notifications"
         ordering = ["-date"]
         db_table = "Notifications"
-
-
-class License(models.Model):
-    key = models.CharField(max_length=100, primary_key=True)
-    active = models.BooleanField(default=True)
-    expires = models.DateTimeField()
-    company_id = models.IntegerField()
-
-    def to_json(self):
-        return {
-            "key": self.key,
-            "active": self.active,
-            "expires": self.expires,
-            "company_id": self.company_id,
-        }
-
-    def __str__(self):
-        return self.key
-
-    class Meta:
-        verbose_name = "License"
-        verbose_name_plural = "Licenses"
-        db_table = "Licenses"
