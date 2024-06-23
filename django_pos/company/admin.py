d5432ae8a8201b278fdc0729d80fc4673ed51dea
Kagema Njoroge <reecejames934@gmail.com>
2024-06-23 17:49:10 +0300

    refactor: Update admin.py to improve user management capabilities
    This commit updates the admin.py file to improve the user management capabilities in the admin interface. It includes changes to the CustomUserAdmin class, adding functionality for activating and deactivating selected users. The `activate` and `deactivate` actions update the `is_active` field of the selected users to True and False, respectively. This enhancement enhances the user management experience in the admin interface.

--- django_pos/company/admin.py
+++ django_pos/company/admin.py
@@ -1,64 +1,41 @@
+from typing import Any
 from django.contrib import admin
+from django.http import HttpRequest
 from .models import Company, Subscription, Branch
 
 
-class BranchAdmin(admin.ModelAdmin):
-    list_display = (
-        "branch_name",
-        "phone_number",
-        "email",
-        "city",
-        "address",
-        "branch_id",
-    )
-    # should not be able to add or change branch details
-
-    # def has_add_permission(self, request):
-    #     return False
-
-    # def has_change_permission(self, request, obj=None):
-    #     return False
-
-    # def has_delete_permission(self, request, obj=None):
-    #     return False
-
-    # def has_module_permission(self, request):
-    #     return False
-
-
-admin.site.register(Branch, BranchAdmin)
-
-
+@admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ("company_name", "phone_number", "currency_symbol")
-    # should not be able to add or change company details
+    list_display = ("company_name", "phone_number", "kra_pin", "city")
 
-    def has_add_permission(self, request):
-        # only one company should be created
-        if Company.objects.count() == 0:
-            return True
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        return Company.objects.all().count() < 1
 
-    # def has_change_permission(self, request, obj=None):
-    #     return False
 
-    def has_delete_permission(self, request, obj=None):
+@admin.register(Branch)
+class BranchAdmin(admin.ModelAdmin):
+    list_display = ("branch_name", "phone_number", "city", "is_headquarter")
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("start_date", "end_date", "is_active")
+    actions = ["deactivate", "activate"]
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
         return False
 
+    def has_change_permission(
+        self, request: HttpRequest, obj: Any | None = ...
+    ) -> bool:
+        return False
 
-class SubscriptionAdmin(admin.ModelAdmin):
-    # should not be able to add or change subscription details
-    list_display = ("company", "start_date", "end_date", "is_active")
+    @admin.action(description="Deactivate subscription")
+    def deactivate(self, request, queryset):
+        queryset.update(is_active=False)
+        self.message_user(request, "Subscription(s) deactivated")
 
-    # comment out for testing
-    # def has_add_permission(self, request):
-    #     return False
-
-    # def has_change_permission(self, request, obj=None):
-    #     return False
-
-    # def has_delete_permission(self, request, obj=None):
-    #     return False
-
-
-admin.site.register(Company, CompanyAdmin)
-admin.site.register(Subscription, SubscriptionAdmin)
+    @admin.action(description="Activate subscription")
+    def activate(self, request, queryset):
+        queryset.update(is_active=True)
+        self.message_user(request, "Subscription(s) activated")
