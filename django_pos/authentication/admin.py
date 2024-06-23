f7973f18a6ef7a3d36d92fa43669fd242ec51d11
Kagema Njoroge <reecejames934@gmail.com>
2024-06-23 17:48:52 +0300

    refactor: Update CustomUserAdmin in admin.py
    This commit updates the CustomUserAdmin class in admin.py to include additional functionality for activating and deactivating selected users. It adds the `activate` and `deactivate` actions, which update the `is_active` field of the selected users to True and False, respectively. This enhancement improves the user management capabilities in the admin interface.

--- django_pos/authentication/admin.py
+++ django_pos/authentication/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    
+    list_display = ("username", "role", "email", "phone_number", "date_of_birth")
+    list_filter = ("role",)
+    search_fields = ("username", "email", "role")
+    list_per_page = 10
+    ordering = ("username",)
+    actions = ["activate", "deactivate"]
+
+    @admin.action(description="Activate selected users")
+    def activate(self, request, queryset):
+        queryset.update(is_active=True)
+        self.message_user(request, "Activated successfully")
+
+    @admin.action(description="Deactivate selected users")
+    def deactivate(self, request, queryset):
+        queryset.update(is_active=False)
+        self.message_user(request, "Deactivated successfully")
