526410ee4d272c9ef9666ba0557ff18e62ed1b16
Kagema Njoroge <reecejames934@gmail.com>
2024-06-23 20:02:14 +0300

    feat: Add CustomUserSerializer for authentication
    This commit adds the CustomUserSerializer to the authentication module. The serializer includes the `password` field as a write-only field, ensuring that the password is not exposed in API responses. This serializer will be used for serializing and deserializing CustomUser objects in the authentication process.

---
+++ django_pos/authentication/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = "__all__"
