07e616176d8280c86ba6ba2199a1ecf38dcb535a
Kagema Njoroge <reecejames934@gmail.com>
2024-06-23 20:02:28 +0300

    refactor: Add serializers for Branch and Subscription models

--- django_pos/company/serializers.py
+++ django_pos/company/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
-from .models import Company
+from .models import Company, Branch, Subscription
 
 
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
+        fields = "__all__"
+
+
+class BranchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Branch
+        fields = "__all__"
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
         fields = "__all__"
