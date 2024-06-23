55ebec5606eb20520a276073ed1fed9586139a34
Kagema Njoroge <reecejames934@gmail.com>
2024-06-23 17:50:15 +0300

    feat: Update NotificationsAdmin in admin.py
    This commit updates the NotificationsAdmin class in admin.py to improve the display and functionality of the admin interface for the Notifications model. It modifies the list_display attribute to include the "title", "message", "date", "read", and "user" fields. It adds a filter for the "read" field and search functionality for the "title", "message", and "user__username" fields. The list_per_page is set to 10 and the list is ordered by the "date" field in descending order. Two new actions, "mark_as_read" and "mark_as_unread", are added to mark selected notifications as read or unread. These changes enhance the user experience and provide more efficient management of notifications in the admin interface.

--- django_pos/pos/admin.py
+++ django_pos/pos/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
 from .models import Notifications
 
-
-class NotificationsAdmin(admin.ModelAdmin):
-    list_display = ("title", "date", "read", "user")
-    list_filter = ("date", "read")
-    search_fields = ("title", "message", "user__username")
-
-
-admin.site.register(Notifications, NotificationsAdmin)
 admin.site.site_header = "Ultimate POS"
 admin.site.site_title = "Ultimate POS Admin"
+admin.site.index_title = "Welcome to Ultimate POS Admin"
+
+
+@admin.register(Notifications)
+class NotificationsAdmin(admin.ModelAdmin):
+    list_display = ("title", "message", "date", "read", "user")
+    list_filter = ("read",)
+    search_fields = ("title", "message", "user__username")
+    list_per_page = 10
+    ordering = ("-date",)
+    actions = ["mark_as_read", "mark_as_unread"]
+
+    @admin.action(description="Mark selected notifications as read")
+    def mark_as_read(self, request, queryset):
+        queryset.update(read=True)
+        # marked as read message
+        self.message_user(request, "Marked as read successfully")
+
+    @admin.action(description="Mark selected notifications as unread")
+    def mark_as_unread(self, request, queryset):
+        queryset.update(read=False)
+        # marked as unread message
+        self.message_user(request, "Marked as unread successfully")
