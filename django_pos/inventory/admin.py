af445a73ab3b7c2860d8ebcd79c3d950643d9014
Kagema Njoroge <reecejames934@gmail.com>
2024-06-23 17:50:05 +0300

    feat: Update InventoryAdmin in admin.py
    This commit updates the InventoryAdmin class in admin.py to improve the display and functionality of the admin interface for the Inventory model. It modifies the list_display attribute to only show the "product" and "quantity" fields. Additionally, it adds search functionality for the "product__name" and "product__description" fields, sets the list_per_page to 10, and orders the list by the "product__name" field. Two new actions, "increase_quantity" and "decrease_quantity", are also added to update the quantity of selected inventory items. These changes enhance the user experience and provide more efficient management of inventory in the admin interface.

--- django_pos/inventory/admin.py
+++ django_pos/inventory/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 from .models import Inventory
+from django.db.models import F
 
 
+@admin.register(Inventory)
 class InventoryAdmin(admin.ModelAdmin):
-    list_display = ['product', 'quantity', 'date_added', 'date_modified']
+    list_display = (
+        "product",
+        "quantity",
+    )
 
+    search_fields = ("product__name", "product__description")
+    list_per_page = 10
+    ordering = ("product__name",)
+    actions = ["increase_quantity", "decrease_quantity"]
 
-admin.site.register(Inventory, InventoryAdmin)
+    @admin.action(description="Increase quantity")
+    def increase_quantity(self, request, queryset):
+        queryset.update(quantity=F("quantity") + 1)
+        self.message_user(request, "Quantity increased successfully")
+
+    @admin.action(description="Decrease quantity")
+    def decrease_quantity(self, request, queryset):
+        queryset.update(quantity=F("quantity") - 1)
+        self.message_user(request, "Quantity decreased successfully")
