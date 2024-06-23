f4eb19df0b5fcbf2ab259a163e782d91c77634a7
Kagema Njoroge <reecejames934@gmail.com>
2024-06-23 17:50:52 +0300

    feat: Improve admin interface for Category and Product models
    This commit enhances the admin interface for the Category and Product models in admin.py. It adds additional functionality and improves the user experience by modifying the list_display, list_filter, search_fields, list_per_page, ordering, and actions attributes for both models. These changes provide better management and organization of categories and products in the admin interface.

--- django_pos/products/admin.py
+++ django_pos/products/admin.py
@@ -1,15 +1,42 @@
 from django.contrib import admin
-
 from .models import Category, Product
 
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("name", "description", "status")
+    list_filter = ("status",)
+    search_fields = ("name", "description")
+    list_per_page = 10
+    ordering = ("name",)
+    actions = ["activate", "deactivate"]
+
+    @admin.action(description="Activate selected categories")
+    def activate(self, request, queryset):
+        queryset.update(status="ACTIVE")
+        self.message_user(request, "Activated successfully")
+
+    @admin.action(description="Deactivate selected categories")
+    def deactivate(self, request, queryset):
+        queryset.update(status="INACTIVE")
+        self.message_user(request, "Deactivated successfully")
 
 
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "category", "price", "status", "track_inventory")
+    list_display = ("name", "description", "track_inventory", "status", "category")
+    list_filter = ("status", "track_inventory", "category")
+    search_fields = ("name", "description", "category__name")
+    list_per_page = 10
+    ordering = ("name",)
+    actions = ["activate", "deactivate"]
 
+    @admin.action(description="Activate selected products")
+    def activate(self, request, queryset):
+        queryset.update(status="ACTIVE")
+        self.message_user(request, "Activated successfully")
 
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Product, ProductAdmin)
+    @admin.action(description="Deactivate selected products")
+    def deactivate(self, request, queryset):
+        queryset.update(status="INACTIVE")
+        self.message_user(request, "Deactivated successfully")
