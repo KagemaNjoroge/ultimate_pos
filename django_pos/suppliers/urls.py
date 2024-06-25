6f2ae962dad35cc5a12c473dc6aff8ddbc427132
Kagema Njoroge <reecejames934@gmail.com>
2024-06-25 22:27:22 +0300

    Add api views urls for suppliers

--- django_pos/suppliers/urls.py
+++ django_pos/suppliers/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register("suppliers", views.SupplierViewSet, basename="suppliers")
 
 app_name = "suppliers"
 
-urlpatterns = [path("", views.index, name="suppliers_index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("new/", views.add_supllier_template, name="new"),
+]
+urlpatterns += router.urls
