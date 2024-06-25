02218ddeb5535abd80dce79812cfe16801d73095
Kagema Njoroge <reecejames934@gmail.com>
2024-06-25 22:28:09 +0300

    Add template view for adding suppliers

--- django_pos/suppliers/views.py
+++ django_pos/suppliers/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Supplier
+from .serializers import SupplierSerializer
 
-from suppliers.models import Supplier
 
-# Create your views here.
+class SupplierViewSet(viewsets.ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
 
 
 def index(request):
     suppliers = Supplier.objects.all()
     return render(request, "suppliers/index.html", {"suppliers": suppliers})
+
+
+def add_supllier_template(request):
+    return render(
+        request,
+        "suppliers/add_supplier.html",
+    )
