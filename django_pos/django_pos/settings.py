5ecae65cfc2683486c9f2fb80b74a4957246fcf0
Kagema Njoroge <reecejames934@gmail.com>
2024-06-25 22:25:40 +0300

    Enable all hosts for development

--- django_pos/django_pos/settings.py
+++ django_pos/django_pos/settings.py
@@ -9,16 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "http://localhost",
-    "http://pos.tomorrow.co.ke",
-    "https://pos.tomorrow.co.ke",
-    "https://localhost",
-    "localhost",
-]
+ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost",
     "http://pos.tomorrow.co.ke",
