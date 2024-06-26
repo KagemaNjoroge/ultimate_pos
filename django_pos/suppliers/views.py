d25f79b359c26599645fac1317c39ce7d32a7662
Kagema Njoroge <reecejames934@gmail.com>
2024-06-26 11:03:15 +0300

    Move Suppliers from suppliers app to api app

--- django_pos/suppliers/views.py
+++ django_pos/suppliers/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
 from .models import Supplier
-from .serializers import SupplierSerializer
-
-
-class SupplierViewSet(viewsets.ModelViewSet):
-    queryset = Supplier.objects.all()
-    serializer_class = SupplierSerializer
 
 
 def index(request):
