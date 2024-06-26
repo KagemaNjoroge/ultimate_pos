df716def539167db82e3f9ed0cb8727fbc3e6740
Kagema Njoroge <reecejames934@gmail.com>
2024-06-26 11:03:30 +0300

    Move Suppliers from suppliers app to api app

--- django_pos/suppliers/urls.py
+++ django_pos/suppliers/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
 from . import views
-from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register("suppliers", views.SupplierViewSet, basename="suppliers")
-
-app_name = "suppliers"
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("new/", views.add_supllier_template, name="new"),
 ]
-urlpatterns += router.urls
