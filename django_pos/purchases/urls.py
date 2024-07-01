e7e5ebb3452b8d89183c3a31520693fa1530c1f7
Kagema Njoroge <reecejames934@gmail.com>
2024-07-01 23:16:32 +0300

    feat: Add new purchase functionality and purchase details page

--- django_pos/purchases/urls.py
+++ django_pos/purchases/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, new_purchase, purchase_details
 
 app_name = "purchases"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("add/", new_purchase, name="add"),
+    path("<int:id>/", purchase_details, name="purchase_details"),
 ]
