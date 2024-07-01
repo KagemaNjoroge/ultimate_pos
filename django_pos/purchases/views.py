f2b056e5622ad21ca385f0fcc830b421b0ca4277
Kagema Njoroge <reecejames934@gmail.com>
2024-07-01 23:16:44 +0300

    feat: Add new purchase functionality and purchase details page

--- django_pos/purchases/views.py
+++ django_pos/purchases/views.py
@@ -1,8 +1,40 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Purchase, Branch, Product, Supplier
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
+    purchases = Purchase.objects.all()
+    return render(
+        request,
+        "purchases/index.html",
+        context={"purchases": purchases},
+    )
 
-    return render(request, "purchases/index.html")
+
+def new_purchase(request):
+    branches = Branch.objects.all()
+    products = Product.objects.all()
+    suppliers = Supplier.objects.all()
+    status = []
+
+    for i in Purchase.status.field.choices:
+        status.append({"value": i[0], "name": i[-1]})
+
+    return render(
+        request,
+        "purchases/new.html",
+        context={
+            "branches": branches,
+            "products": products,
+            "suppliers": suppliers,
+            "status": status,
+        },
+    )
+
+
+def purchase_details(request, id):
+    purchase = get_object_or_404(Purchase, id=id)
+    return render(
+        request, "purchases/purchase-details.html", context={"purchase": purchase}
+    )
