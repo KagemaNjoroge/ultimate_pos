e2f163af6b2c86839f6ea7eae28e05531547fed8
Kagema Njoroge <reecejames934@gmail.com>
2024-07-06 23:51:59 +0300

    ```text feat: Add expense and expense category creation routes
    New routes have been added to handle the creation of expenses and expense categories. This addition improves the functionality of the application by providing dedicated pages for creating new expenses and expense categories.

--- django_pos/expenses/urls.py
+++ django_pos/expenses/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import expense_categories, expenses
+from .views import expense_categories, expenses, add_expense, add_expense_category
 
 app_name = "expenses"
 
@@ -7,5 +7,7 @@ app_name = "expenses"
 urlpatterns = [
     path("", expenses, name="index"),
     path("category/", expense_categories, name="category"),
+    path("add/", add_expense, name="add_expense"),
     path("category/<int:id>/", expense_categories, name="category_operations"),
+    path("add-category/", add_expense_category, name="add_expense_category"),
 ]
