ae9d8880e952ae548a49d911b0143494f914da8c
Kagema Njoroge <reecejames934@gmail.com>
2024-07-06 21:52:17 +0300

    Refactor expenses URLs for consistency and clarity

--- django_pos/expenses/urls.py
+++ django_pos/expenses/urls.py
@@ -1,16 +1,12 @@
 from django.db import router
 from django.urls import path
-from .views import expense_categories, ExpenseView
-
+from .views import expense_categories, expenses
 
 app_name = "expenses"
 
 
 urlpatterns = [
-    # expenses
-    path("", ExpenseView.as_view(), name="index"),
-    path("<int:pk>", ExpenseView.as_view(), name="expense_operations"),
-    # expense categories
-    path("category", expense_categories, name="category"),
-    path("category/<int:id>", expense_categories, name="category_operations"),
+    path("", expenses, name="index"),
+    path("category/", expense_categories, name="category"),
+    path("category/<int:id>/", expense_categories, name="category_operations"),
 ]
