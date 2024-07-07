efd862fe8733e455530f58827387e705fb58fc21
Kagema Njoroge <reecejames934@gmail.com>
2024-07-08 01:33:14 +0300

    feat: Add routes and templates for editing expenses and expense categories
    New routes and templates have been added to handle the editing of expenses and expense categories. This addition improves the functionality of the application by providing dedicated pages for editing existing expenses and expense categories.

--- django_pos/expenses/urls.py
+++ django_pos/expenses/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
-from .views import expense_categories, expenses, add_expense, add_expense_category
+from .views import (
+    expense_categories,
+    expenses,
+    add_expense,
+    add_expense_category,
+    edit_expense,
+    edit_category,
+)
 
 app_name = "expenses"
 
 
 urlpatterns = [
     path("", expenses, name="index"),
+    path("edit/<int:id>/", edit_expense, name="edit_expense"),
     path("category/", expense_categories, name="category"),
+    path("category/edit/<int:id>/", edit_category, name="edit_category"),
     path("add/", add_expense, name="add_expense"),
     path("category/<int:id>/", expense_categories, name="category_operations"),
     path("add-category/", add_expense_category, name="add_expense_category"),
