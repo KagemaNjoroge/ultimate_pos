325c432618eff2aef23a37b99375b3e5901cdf70
Kagema Njoroge <reecejames934@gmail.com>
2024-07-09 17:52:29 +0300

    feat: Add "Add Branch" functionality to Django POS
    This commit adds the "Add Branch" functionality to the Django POS application. It includes the necessary changes to the `urls.py` file to add a new URL path for adding a branch. This addition enhances the functionality of the application by allowing users to add new branches through the user interface.

--- django_pos/company/urls.py
+++ django_pos/company/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import CompanyView, index, branches, invoice_design
+from .views import CompanyView, index, branches, invoice_design, add_branch
 
 app_name = "company"
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path("<int:id>/", CompanyView.as_view(), name="company_id"),
     path("settings/", view=index, name="settings"),
     path("branches/", view=branches, name="branches"),
+    path("branches/add/", view=add_branch, name="add_branch"),
     path("invoice-design/", view=invoice_design, name="invoice_design"),
 ]
