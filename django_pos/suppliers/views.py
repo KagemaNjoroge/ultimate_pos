30cffeb7009e166e3f8cc4c27f67da430c4e0b4b
Kagema Njoroge <reecejames934@gmail.com>
2024-07-09 17:58:29 +0300

    refactor: Add supplier details view
    This commit adds a new view function, `supplier_details`, to the `suppliers/views.py` file. The view function retrieves a `Supplier` object based on the provided `id` parameter and renders the `supplier_details.html` template with the supplier data. This addition enhances the functionality of the application by allowing users to view the details of a specific supplier.

--- django_pos/suppliers/views.py
+++ django_pos/suppliers/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Supplier
 from company.models import Branch
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
@@ -13,3 +14,8 @@ def add_supllier_template(request):
     return render(
         request, "suppliers/add_supplier.html", context={"branches": branches}
     )
+
+
+def supplier_details(request, id):
+    supplier = get_object_or_404(Supplier, id=id)
+    return render(request, "suppliers/supplier_details.html", {"supplier": supplier})
