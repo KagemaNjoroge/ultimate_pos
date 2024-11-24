1bd986caed7639e12412cc449501b3e43fbf4a41
Kagema Njoroge <reecejames934@gmail.com>
2024-11-24 13:19:32 +0300

    chore: remove unused ETIMS URL patterns

--- django_pos/etims/urls.py
+++ django_pos/etims/urls.py
@@ -6,11 +6,5 @@ from . import views
 app_name = "etims"
 urlpatterns = [
     path("", views.etims, name="etims"),
-    path("branches/", views.get_etims_branches_list, name="branches"),
-    path("notices/", views.get_etims_notices, name="notices"),
-    path(
-        "item-class-codes/", views.get_etims_items_class_codes, name="item-class-codes"
-    ),
-    path("items-list/", views.get_etims_items_list, name="items-list"),
     path("server-status/", views.get_etims_server_status, name="server-status"),
 ]
