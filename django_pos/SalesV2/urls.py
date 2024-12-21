19e5402f67050d71f741c38e45d4432dc5cd9799
KagemaNjoroge <reecejames934@gmail.com>
2024-12-21 08:03:27 +0300

    Remove unnecessary SalesV2 app

--- django_pos/SalesV2/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import SaleViewSet, SaleItemViewSet
-
-
-router = DefaultRouter()
-router.register(r"sales", SaleViewSet)
-router.register(r"sale-items", SaleItemViewSet)
-
-urlpatterns = router.urls
