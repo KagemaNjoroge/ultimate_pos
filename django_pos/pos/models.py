e711de24e834593747304be20d5ba2eb4582e2ba
KagemaNjoroge <reecejames934@gmail.com>
2024-12-23 18:18:48 +0300

    Refactor Notifications model to use get_user_model for user foreign key

--- django_pos/pos/models.py
+++ django_pos/pos/models.py
@@ -1,14 +1,13 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth import get_user_model
 
 
-# Create your models here.
 class Notifications(models.Model):
     title = models.CharField(max_length=100)
     message = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     read = models.BooleanField(default=False)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
