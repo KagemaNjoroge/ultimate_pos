030db4eea763bb838f221d6daede812b927e450f
KagemaNjoroge <reecejames934@gmail.com>
2024-12-23 18:18:53 +0300

    Remove unused subscription page URL from urlpatterns

--- django_pos/pos/urls.py
+++ django_pos/pos/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path("pos/", views.pos, name="pos"),
     path("notifications/", views.get_notifications, name="notifications"),
     path("notifications/<int:id>/", views.get_notifications, name="notifications"),
-    path("subscription/", view=views.subscription_page, name="subscription_page"),
     path("management/", view=views.register_company, name="register_company"),
     path("checkout/<int:amount>/", view=views.checkout, name="checkout"),
     path("packages/", views.fetch_packages, name="fetch_packages"),
