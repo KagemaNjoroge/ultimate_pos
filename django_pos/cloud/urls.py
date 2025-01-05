49b279d425bfa2ed30e97bb79e15bc89ee89b359
Kagema Njoroge <reecejames934@gmail.com>
2025-01-05 09:58:54 +0300

    Remove cloud app and all associated files from the project

--- django_pos/cloud/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-app_name = "cloud"
-
-from . import views
-
-
-urlpatterns = [path("", views.index, name="cloud")]
--- django_pos/cloud/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-# Create your views here.
-
-
-@login_required(login_url="/users/login/")
-def index(request):
-    return render(request, "cloud/index.html")
