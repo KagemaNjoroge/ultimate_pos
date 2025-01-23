b463f561261b5efc023f3ab3bdd1244c4ac5ed5a
Kagema Njoroge <reecejames934@gmail.com>
2025-01-23 09:00:39 +0300

    Remove unnecessary payments configurations

--- django_pos/pos/payments.py
+++
@@ -1,9 +0,0 @@
-from intasend import APIService
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-token = os.getenv("INTASEND_SECRET_API_KEY ")
-publishable_key = os.getenv("INTASEND_PUBLIC_API_KEY")
-service = APIService(token=token, publishable_key=publishable_key, test=True)
