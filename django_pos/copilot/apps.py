0fa59aaa62b99ef3d56364950c405c4b5d1e39c5
kagema <reecejames934@gmail.com>
2025-06-12 18:59:02 +0300

    Remove unncessary app

--- django_pos/copilot/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CopilotConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'copilot'
--- django_pos/copilot/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import chat
-
-app_name = "copilot"
-
-urlpatterns = [
-    path("chat/", chat, name="chat"),
-]
