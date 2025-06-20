56da9800c8a1fe10d22d31664d960d099c933caf
kagema <reecejames934@gmail.com>
2025-06-20 17:57:28 +0300

    Update CustomerAdmin list_display to show relevant fields

--- django_pos/customers/admin.py
+++ django_pos/customers/admin.py
@@ -4,7 +4,7 @@ from .models import Customer
 
 
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('kra_pin', 'first_name', 'last_name', 'phone')
+    list_display = ("first_name", "last_name", "phone")
 
 
 admin.site.register(Customer, CustomerAdmin)
