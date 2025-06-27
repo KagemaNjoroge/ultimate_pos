2b56e227c06087f38cd53dd72bdf5986ce4e8570
kagema <reecejames934@gmail.com>
2025-06-27 11:15:35 +0300

    Remove SubscriptionAdmin and update CompanyAdmin list_display

--- django_pos/company/admin.py
+++ django_pos/company/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.http import HttpRequest
-from .models import Company, Subscription, Branch
+from .models import Company, Branch
 
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ("company_name", "phone_number", "kra_pin", "city")
+    list_display = ("company_name", "phone_number", "city")
 
     def has_add_permission(self, request: HttpRequest) -> bool:
         return Company.objects.all().count() < 1
@@ -28,26 +28,3 @@ class BranchAdmin(admin.ModelAdmin):
         "is_headquarter",
     )
 
-
-@admin.register(Subscription)
-class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ("start_date", "end_date", "is_active")
-    actions = ["deactivate", "activate"]
-
-    # def has_add_permission(self, request: HttpRequest) -> bool:
-    #     return False
-
-    # def has_change_permission(
-    #     self, request: HttpRequest, obj: Any | None = ...
-    # ) -> bool:
-    #     return False
-
-    @admin.action(description="Deactivate subscription")
-    def deactivate(self, request, queryset):
-        queryset.update(is_active=False)
-        self.message_user(request, "Subscription(s) deactivated")
-
-    @admin.action(description="Activate subscription")
-    def activate(self, request, queryset):
-        queryset.update(is_active=True)
-        self.message_user(request, "Subscription(s) activated")
