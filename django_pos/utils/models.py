666abd67f3e28035e7297bbb714df92dac048ef0
kagema <reecejames934@gmail.com>
2025-06-27 11:15:14 +0300

    Refactor models.py to improve formatting and add TimestampedModel for reuse

--- django_pos/utils/models.py
+++ django_pos/utils/models.py
@@ -1,14 +1,26 @@
 from django.db import models
 
+
 class Photo(models.Model):
     image = models.ImageField(upload_to="photos", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        
+
         verbose_name_plural = "Photos"
         verbose_name = "Photo"
 
     def __str__(self) -> str:
-        return f"Photo {self.id}" 
+        return f"Photo {self.id}"
+
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self) -> str:
+        return f"Created at {self.created_at}, Updated at {self.updated_at}"
