77836bbbd3806ef1640f7a7946c9e88a738c7f1b
kagema <reecejames934@gmail.com>
2025-06-29 15:21:07 +0300

    Add Permission model admin and enhance CustomUser admin interface with fieldsets

--- django_pos/authentication/admin.py
+++ django_pos/authentication/admin.py
@@ -1,16 +1,45 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, Permission
+
+
+@admin.register(Permission)
+class PermissionAdmin(admin.ModelAdmin):
+    list_display = ("code", "name", "created_at", "updated_at")
+    search_fields = ("code", "name")
+    list_per_page = 10
+    ordering = ("code",)
+    readonly_fields = ("created_at", "updated_at")
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    
+
     list_display = ("username", "role", "email", "phone_number", "date_of_birth")
     list_filter = ("role",)
     search_fields = ("username", "email", "role")
     list_per_page = 10
     ordering = ("username",)
     actions = ["activate", "deactivate"]
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal Info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "phone_number",
+                    "address",
+                    "date_of_birth",
+                    "id_number",
+                )
+            },
+        ),
+        ("Permissions", {"fields": ("role", "branch", "permissions")}),
+        ("Profile Picture", {"fields": ("profile_pic",)}),
+        ("Status", {"fields": ("is_active",)}),
+    )
 
     @admin.action(description="Activate selected users")
     def activate(self, request, queryset):
