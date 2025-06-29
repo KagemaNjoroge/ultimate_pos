af837b8bcd52db5f82fec79950f74107cebd7d59
kagema <reecejames934@gmail.com>
2025-06-29 15:20:26 +0300

    Add Permission model and enhance CustomUser with branch and permissions fields

--- django_pos/authentication/models.py
+++ django_pos/authentication/models.py
@@ -1,22 +1,50 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from company.models import Branch
+from utils.models import TimestampedModel
+
+
+class Permission(TimestampedModel):
+    """
+    Example: 'make_sales', 'view_reports', 'manage_inventory', 'view_all_branches'
+    """
+
+    code = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class CustomUser(AbstractUser):
+    """ "
+    Roles:
+      - Cashier: Can handle transactions and sales.
+      - Branch Manager: Can manage the branch operations.
+      - Admin: Has full access to the system, including user management.
+      - Inventory Manager: Can manage inventory and stock levels.
+    """
+
     ROLES = (
         (
             "Cashier",
             "Cashier",
         ),
         (
-            "Manager",
-            "Manager",
+            "InventoryManager",
+            "InventoryManager",
         ),
         (
             "Admin",
             "Admin",
         ),
+        (
+            "BranchManager",
+            "BranchManager",
+        ),
     )
+    branch = models.ForeignKey(Branch, on_delete=models.SET_NULL, null=True, blank=True)
+    permissions = models.ManyToManyField(Permission, blank=True)
     role = models.CharField(max_length=50, choices=ROLES, default="Cashier")
     phone_number = models.CharField(max_length=15, null=True, blank=True)
     address = models.TextField(null=True, blank=True)
@@ -31,3 +59,6 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+    def has_perm_code(self, code):
+        return self.permissions.filter(code=code).exists()
