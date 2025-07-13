fb313e9e73543b879df604e972c67c6a86d73921
kagema <reecejames934@gmail.com>
2025-07-13 16:23:44 +0300

    Add company creation logic to CompanyView and update URL patterns

--- django_pos/company/urls.py
+++ django_pos/company/urls.py
@@ -20,6 +20,7 @@ router.register(r"branch/api", BranchesViewSet, basename="branch")
 
 urlpatterns = [
     path("api/<int:id>/", CompanyView.as_view(), name="company_id"),
+    path("api/", CompanyView.as_view(), name="company"),
     path("settings/", view=index, name="settings"),
     path("branches/", view=branches, name="branches"),
     path("branches/add/", view=add_branch, name="add_branch"),
