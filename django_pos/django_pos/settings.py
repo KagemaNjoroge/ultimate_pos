d4f1fd493986c219682561d2268c0fb4010c2389
kagema <reecejames934@gmail.com>
2025-07-13 15:10:55 +0300

    Add Django Filter backend to REST framework settings

--- django_pos/django_pos/settings.py
+++ django_pos/django_pos/settings.py
@@ -191,6 +191,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
 }
 # JWT
 SIMPLE_JWT = {
