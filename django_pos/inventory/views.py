8cc0304a9779f8f11203dbd5bb8006de276c6262
kagema <reecejames934@gmail.com>
2025-07-14 20:02:34 +0300

    Enhance inventory views: filter inventory by branch and display current branch info in template

--- django_pos/inventory/views.py
+++ django_pos/inventory/views.py
@@ -1,3 +1,4 @@
+from company.utils.branch_utils import get_current_branch
 from .serializers import InventorySerializer
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, get_object_or_404
@@ -17,16 +18,38 @@ class InventoryViewSet(ModelViewSet):
 
 @login_required()
 def index(request):
-    inventory = Inventory.objects.all()
-    context = {"inventory": inventory}
+    current_branch = get_current_branch(request)
+
+    # check if there is a branch query parameter
+    if request.GET.get("branch"):
+        branch_id = request.GET.get("branch")
+        current_branch = Branch.objects.filter(id=branch_id).first()
+
+    inventory = (
+        Inventory.objects.filter(branch=current_branch)
+        if current_branch
+        else Inventory.objects.all()
+    )
+    context = {"inventory": inventory, "current_branch": current_branch}
     return render(request, "inventory/inventory.html", context)
 
 
 @login_required()
 @require_http_methods(["GET"])
 def add_inventory(request: HttpRequest) -> HttpResponse:
+    current_branch = get_current_branch(request)
 
-    products = Product.objects.exclude(inventory__isnull=False)
+    # check if there is a branch query parameter
+    if request.GET.get("branch"):
+        branch_id = request.GET.get("branch")
+        current_branch = Branch.objects.filter(id=branch_id).first()
+
+    if current_branch:
+        # get only products that are not already in inventory for the current branch
+        products = Product.objects.exclude(inventory__branch=current_branch)
+    else:
+        products = Product.objects.all()
+
     branches = Branch.objects.all()
     return render(
         request,
