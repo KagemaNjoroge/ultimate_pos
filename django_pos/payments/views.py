9c098f88cffa27633f4109106c68384974186b81
kagema <reecejames934@gmail.com>
2025-07-14 20:19:55 +0300

    Add check_payment_status view to retrieve payment details

--- django_pos/payments/views.py
+++ django_pos/payments/views.py
@@ -1,1 +1,20 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Payment
 
+
+@api_view(["POST"])
+def check_payment_status(request, id):
+    payment = get_object_or_404(Payment, id=id)
+
+    return Response(
+        {
+            "id": payment.id,
+            "amount": str(payment.amount),
+            "currency": payment.currency,
+            "payment_method": payment.payment_method,
+            "reference": payment.reference,
+            "status": payment.status,
+        }
+    )
