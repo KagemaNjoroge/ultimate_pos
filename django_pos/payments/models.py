dcb8c77ffbda9e3d5afe015dff1e1a71919d05e8
kagema <reecejames934@gmail.com>
2025-07-16 09:36:39 +0300

    Add payment method choices and sale foreign key to Payment model

--- django_pos/payments/models.py
+++ django_pos/payments/models.py
@@ -1,5 +1,6 @@
 from utils.models import TimestampedModel
 from django.db import models
+from sales.models import Sale
 
 
 class Payment(TimestampedModel):
@@ -7,9 +8,17 @@ class Payment(TimestampedModel):
     Represents a payment made by a customer.
     """
 
+    payment_methods = (
+        ("cash", "Cash"),
+        ("mpesa", "M-Pesa"),
+        ("credit_card", "Credit Card"),  # visa & mastercard
+    )
+    sale = models.ForeignKey(
+        Sale, on_delete=models.CASCADE, related_name="payments", blank=True, null=True
+    )
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     currency = models.CharField(max_length=3)
-    payment_method = models.CharField(max_length=50)
+    payment_method = models.CharField(max_length=20, choices=payment_methods)
     reference = models.CharField(max_length=100, unique=True)
     status = models.CharField(
         max_length=20,
