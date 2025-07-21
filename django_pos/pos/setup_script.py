c381baf75316a1fee90cf280704cdcca0ff46595
kagema <reecejames934@gmail.com>
2025-07-21 10:52:50 +0300

    chore: improve formatting and readability of tax rates documentation in setup_tax_groups function

--- django_pos/pos/setup_script.py
+++ django_pos/pos/setup_script.py
@@ -30,14 +30,17 @@ def create_demo_branch(company: Company):
 def setup_tax_groups():
     """
 
-        There are two (2) tax rates:-
+    There are two (2) tax rates:-
 
-        16% (General rate) - this rate applies to all taxable goods and taxable services other than zero-rated supplies.
-        0% (Zero-rate) - this rate applies to specific supplies listed in the Second Schedule to the VAT Act, 2013.
+    16% (General rate) - this rate applies to all taxable goods and taxable services other than zero-rated supplies.
+    0% (Zero-rate) - this rate applies to specific supplies listed in the Second Schedule to the VAT Act, 2013.
 
-    Note: 8% (Other rate) - This rate applied to certain supplies (petroleum products) prior to 1st July 2023 but was deleted by the Finance Act, 2023.
+    Note: 8% (Other rate) - This rate applied to certain supplies (petroleum products) prior to 1st July 2023
+    but was deleted by the Finance Act, 2023.
 
-    Exempt supplies are not taxable supplies and any related input tax is therefore not deductible. Exempt supplies are listed in the First Schedule to the VAT Act 2013. Taxpayers who only make exempt supplies are not required to register for VAT.
+    Exempt supplies are not taxable supplies and any related input tax is therefore not deductible.
+    Exempt supplies are listed in the First Schedule to the VAT Act 2013. Taxpayers who only make exempt
+    supplies are not required to register for VAT.
 
     """
     tax_groups = [
@@ -51,19 +54,24 @@ def setup_tax_groups():
             "name": "Zero Rate",
             "tax_rate": 0.0,
             "status": "ACTIVE",
-            "description": "Zero-rated supplies are taxable supplies on which the tax rate is 0%. Zero-rated supplies are listed in the Second Schedule to the VAT Act, 2013. Taxpayers who only make zero-rated supplies are not required to register for VAT.",
+            "description": """Zero-rated supplies are taxable supplies on which the tax rate is 0%. 
+            Zero-rated supplies are listed in the Second Schedule to the VAT Act, 2013. 
+            Taxpayers who only make zero-rated supplies are not required to register for VAT.""",
         },
         {
             "name": "Exempt",
             "tax_rate": 0.0,
             "status": "ACTIVE",
-            "description": "Exempt supplies are not taxable supplies and any related input tax is therefore not deductible. Exempt supplies are listed in the First Schedule to the VAT Act 2013. Taxpayers who only make exempt supplies are not required to register for VAT.",
+            "description": """Exempt supplies are not taxable supplies and any related input tax is therefore not deductible.
+              Exempt supplies are listed in the First Schedule to the VAT Act 2013. 
+              Taxpayers who only make exempt supplies are not required to register for VAT.""",
         },
         {
             "name": "Other Rate",
             "tax_rate": 8.0,
             "status": "INACTIVE",
-            "description": "This rate applied to certain supplies (petroleum products) prior to 1st July 2023 but was deleted by the Finance Act, 2023.",
+            "description": """This rate applied to certain supplies (petroleum products) 
+            prior to 1st July 2023 but was deleted by the Finance Act, 2023.""",
         },
     ]
     for tax_group in tax_groups:
